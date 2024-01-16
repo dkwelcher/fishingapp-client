04024697faab729074b6f04fd14c0f96fd2958cd
dkwelcher <dkwelcher@gmail.com>
2024-01-16 11:29:32 -0500

    Modified view div to remove paddding & use all view space

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -5,7 +5,7 @@ function Layout() {
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
       <Sidebar />
-      <div className="p-4">
+      <div className="flex-col flex-1">
         {/* <div>header</div> */}
         <div>{<Outlet />}</div>
       </div>
