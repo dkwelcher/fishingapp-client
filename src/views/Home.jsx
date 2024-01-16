538210ffb03e86d851e9aa85d6e5c27c08bb93d0
dkwelcher <dkwelcher@gmail.com>
2024-01-16 11:30:07 -0500

    Added search bar & bg image & styles

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,11 @@
+import SearchBar from "../components/SearchBar";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex justify-center items-center h-screen  bg-home-image bg-cover bg-center">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default Home;
