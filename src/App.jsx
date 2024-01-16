c5f984668cf038d97b3193f153bd79124c279571
dkwelcher <dkwelcher@gmail.com>
2024-01-16 10:29:01 -0500

    Added Routes for all views

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/shared/Layout";
+import Home from "./views/Home";
+import ViewTrip from "./views/ViewTrip";
+import AddTrip from "./views/AddTrip";
+import EditTrip from "./views/EditTrip";
+import DeleteTrip from "./views/DeleteTrip";
+import LandingPage from "./views/LandingPage";
+import Signup from "./views/Signup";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/view-trip" element={<ViewTrip />} />
+            <Route path="/add-trip" element={<AddTrip />} />
+            <Route path="/edit-trip" element={<EditTrip />} />
+            <Route path="/delete-trip" element={<DeleteTrip />} />
+          </Route>
+          <Route path="/landing" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<div>Login</div>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
