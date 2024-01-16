4e559346f39ed9758cfdbd47b7fb2ed31b89321b
dkwelcher <dkwelcher@gmail.com>
2024-01-16 10:33:56 -0500

    Added Layout component for dashboard design.
    Sidebar is fixed left of screen, and views will display to its right.

---
+++ src/components/shared/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+function Layout() {
+  return (
+    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+      <Sidebar />
+      <div className="p-4">
+        {/* <div>header</div> */}
+        <div>{<Outlet />}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
