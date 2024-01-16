16b232ecc499fb3ce6cbf3e15472345c214f8d40
dkwelcher <dkwelcher@gmail.com>
2024-01-16 14:26:05 -0500

    Added ViewCard & code for simple viewing of catch data in ViewTrips

---
+++ src/components/ViewCard.jsx
@@ -0,0 +1,34 @@
+function ViewCard({ catches }) {
+  return (
+    <div>
+      {catches.map((catchItem, i) => (
+        <div className="bg-cyan-600 mb-2 p-2 rounded-md">
+          <div className="grid grid-cols-9 border-b border-b-black mb-2">
+            <p>Time</p>
+            <p>Species</p>
+            <p>Latitude</p>
+            <p>Longitude</p>
+            <p>Bait</p>
+            <p>Weather</p>
+            <p>Air Temp</p>
+            <p>Water Temp</p>
+            <p>Wind Speed</p>
+          </div>
+          <div className="grid grid-cols-9">
+            <p>{catchItem.time}</p>
+            <p>{catchItem.fish}</p>
+            <p>{catchItem.latitude}</p>
+            <p>{catchItem.longitude}</p>
+            <p>{catchItem.bait}</p>
+            <p>{catchItem.weather}</p>
+            <p>{catchItem.airTemp}</p>
+            <p>{catchItem.waterTemp}</p>
+            <p>{catchItem.windSpeed}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ViewCard;
