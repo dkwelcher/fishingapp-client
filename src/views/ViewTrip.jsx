84058a610b7741512f3f1b9d72247e061affec4f
dkwelcher <dkwelcher@gmail.com>
2024-01-21 19:22:37 -0500

    Added hero image, adjusted width & padding, changed TripSearch to DateSearch

--- src/views/ViewTrip.jsx
+++ src/views/ViewTrip.jsx
@@ -1,4 +1,5 @@
-import TripSearch from "../components/TripSearch";
+// import TripSearch from "../components/TripSearch";
+import DateSearch from "../components/DateSearch";
 import { useState } from "react";
 import ViewCard from "../components/ViewCard";
 
@@ -17,9 +18,9 @@ function ViewTrip() {
   const [catches] = useState([
     {
       fish: "Striped Bass",
-      time: "6:30",
-      latitude: "30.0",
-      longitude: "-90.0",
+      time: "5:30",
+      latitude: "33.691743",
+      longitude: "-82.217761",
       bait: "Blueback Herring",
       weather: "cloudy",
       airTemp: "65",
@@ -28,9 +29,9 @@ function ViewTrip() {
     },
     {
       fish: "Striped Bass",
-      time: "6:30",
-      latitude: "30.0",
-      longitude: "-90.0",
+      time: "5:36",
+      latitude: "32.259143",
+      longitude: "-81.198345",
       bait: "Blueback Herring",
       weather: "cloudy",
       airTemp: "65",
@@ -39,9 +40,20 @@ function ViewTrip() {
     },
     {
       fish: "Striped Bass",
-      time: "6:30",
-      latitude: "30.0",
-      longitude: "-90.0",
+      time: "5:42",
+      latitude: "33.613486",
+      longitude: "-82.991645",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "65",
+      waterTemp: "60",
+      windSpeed: "8",
+    },
+    {
+      fish: "Channel Catfish",
+      time: "5:44",
+      latitude: "33.613589",
+      longitude: "-82.992346",
       bait: "Blueback Herring",
       weather: "cloudy",
       airTemp: "65",
@@ -50,45 +62,111 @@ function ViewTrip() {
     },
     {
       fish: "Striped Bass",
-      time: "6:30",
-      latitude: "30.0",
-      longitude: "-90.0",
+      time: "6:45",
+      latitude: "34.513486",
+      longitude: "-83.543168",
       bait: "Blueback Herring",
       weather: "cloudy",
-      airTemp: "65",
+      airTemp: "68",
+      waterTemp: "60",
+      windSpeed: "8",
+    },
+    {
+      fish: "Largemouth Bass",
+      time: "6:47",
+      latitude: "34.513246",
+      longitude: "-83.549476",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "68",
       waterTemp: "60",
       windSpeed: "8",
     },
     {
       fish: "Striped Bass",
-      time: "6:30",
-      latitude: "30.0",
-      longitude: "-90.0",
+      time: "8:26",
+      latitude: "36.761389",
+      longitude: "-86.498315",
       bait: "Blueback Herring",
       weather: "cloudy",
-      airTemp: "65",
-      waterTemp: "60",
+      airTemp: "72",
+      waterTemp: "61",
+      windSpeed: "8",
+    },
+    {
+      fish: "Channel Catfish",
+      time: "8:28",
+      latitude: "36.761389",
+      longitude: "-86.498315",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "72",
+      waterTemp: "61",
+      windSpeed: "8",
+    },
+    {
+      fish: "Striped Bass",
+      time: "8:29",
+      latitude: "35.984351",
+      longitude: "-86.986479",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "72",
+      waterTemp: "61",
+      windSpeed: "8",
+    },
+    {
+      fish: "Striped Bass",
+      time: "8:42",
+      latitude: "36.846388",
+      longitude: "-86.912549",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "74",
+      waterTemp: "61",
+      windSpeed: "8",
+    },
+    {
+      fish: "Striped Bass",
+      time: "8:53",
+      latitude: "36.761389",
+      longitude: "-86.498315",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "74",
+      waterTemp: "61",
+      windSpeed: "8",
+    },
+    {
+      fish: "Striped Bass",
+      time: "8:59",
+      latitude: "36.761389",
+      longitude: "-86.998315",
+      bait: "Blueback Herring",
+      weather: "cloudy",
+      airTemp: "74",
+      waterTemp: "61",
       windSpeed: "8",
     },
   ]);
 
   return (
-    <div className="flex flex-col justify-center items-left mx-[10%] my-4 p-8 rounded-md bg-slate-200 text-slate-800">
-      <div className="flex flex-col justify-center items-left mb-4">
+    <div className="flex flex-col justify-center items-left text-slate-800">
+      <div className="flex flex-col justify-center items-left px-10 py-20 mb-4 bg-viewtrip-image bg-cover bg-center text-white">
         <h1 className="font-title font-medium text-4xl mb-4">
           Find Your Recorded Fishing Trips
         </h1>
-        <p className="w-[60%] font-paragraph text-md mb-8 text-slate-500">
+        <p className="w-[40%] font-paragraph text-md mb-8">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint tempora
           doloremque dolore nam, laborum maiores cupiditate inventore natus
           provident odit ipsum est, necessitatibus deleniti amet.
         </p>
         <div>
-          <TripSearch getTripInfo={getTripInfo} />
+          <DateSearch getTripInfo={getTripInfo} />
         </div>
       </div>
 
-      <div className="bg-gradient-to-b from-slate-800 to-slate-400 p-2 rounded-md">
+      <div className="w-[95%] mx-auto mb-4 bg-gradient-to-b from-slate-800 to-slate-400 p-4 rounded-md">
         <div
           className={`w-[500px] h-[500px] bg-green-200 m-auto ${
             trip && catches.length > 0 ? "hidden" : ""
