1ffec473384a2cf11dcdbbd8d8a71ca3c11f86fc
dkwelcher <dkwelcher@gmail.com>
2024-01-29 16:38:15 -0500

    Abstracted from CRUD-related views into component
    Takes parameters from the views (ViewTrip, AddTrip, EditTrip, & DeleteTrip) & renders the informational section of the page.

---
+++ src/components/views/InfoSection.jsx
@@ -0,0 +1,17 @@
+import DateSearch from "../DateSearch";
+
+function InfoSection({ getTripInfo, backgroundImage, title, description }) {
+  return (
+    <div
+      className={`flex flex-col justify-center items-left px-10 py-20 mb-8 bg-cover bg-center text-white ${backgroundImage}`}
+    >
+      <h1 className="font-title font-medium text-4xl mb-4">{title}</h1>
+      <p className="w-[40%] font-paragraph text-md mb-8">{description}</p>
+      <div>
+        <DateSearch getTripInfo={getTripInfo} />
+      </div>
+    </div>
+  );
+}
+
+export default InfoSection;
