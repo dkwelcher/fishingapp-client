18c34d7f1b64bb8734393fe3a2f48b272fb128d9
dkwelcher <dkwelcher@gmail.com>
2024-01-29 14:10:17 -0500

    Added login-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         "hero-image": "url('./src/assets/striped-bass-school.png')",
         "callout-image": "url('./src/assets/boat.png')",
         "signup-image": "url('./src/assets/red-drum.png')",
+        "login-image": "url('./src/assets/trout.png')",
         "home-image": "url('./src/assets/sunset.png')",
         "viewtrip-image": "url('./src/assets/mountain-lake.png')",
         "transparent-shadow": "linear-gradient(rgb(0 0 0/40%) 0 0)",
