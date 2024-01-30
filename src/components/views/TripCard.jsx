2c4b273e2198f5e28bb1e39e3a9a771c85d9017d
dkwelcher <dkwelcher@gmail.com>
2024-01-30 12:02:16 -0500

    Added btns to manage trip. Adjusted cond. rendering logic

--- src/components/views/TripCard.jsx
+++ src/components/views/TripCard.jsx
@@ -4,24 +4,42 @@ function TripCard({ trip, catches }) {
   return (
     <div
       className={`w-[95%] mx-auto mb-4 p-4 rounded-md ${
-        trip && catches.length > 0
+        trip.location && trip.date
           ? "bg-gradient-to-b from-slate-800 to-slate-400"
           : ""
       }`}
     >
       <div
         className={`w-[500px] h-[500px] bg-green-200 m-auto ${
-          trip && catches.length > 0 ? "hidden" : ""
+          trip.location && trip.date ? "hidden" : ""
         }`}
       ></div>
-      <div className="flex justify-between mb-4 text-white">
-        {trip.location && (
-          <h2 className="font-title text-2xl">{trip.location}</h2>
-        )}
-        {trip.date && <h2 className="font-title text-2xl">{trip.date}</h2>}
+      <div className="mb-8 flex justify-between">
+        <div className="flex text-white">
+          {trip.location && (
+            <h2 className="font-title font-semibold text-5xl">
+              {trip.location}
+            </h2>
+          )}
+          {trip.date && (
+            <h2 className="font-title ml-6 text-2xl">{trip.date}</h2>
+          )}
+        </div>
+        <div
+          className={`flex items-center ${
+            trip.location && trip.date ? "" : "hidden"
+          }`}
+        >
+          <button className="bg-white text-slate-600 px-6 py-2 rounded-sm font-paragraph font-bold">
+            Edit Current Trip Info
+          </button>
+          <button className="ml-4 bg-white text-slate-600 px-6 py-2 rounded-sm font-paragraph font-bold">
+            Delete Current Trip
+          </button>
+        </div>
       </div>
-      <div className="px-4">
-        {catches && catches.length > 0 && <CatchCard catches={catches} />}
+      <div className="px-6">
+        {trip.location && trip.date ? <CatchCard catches={catches} /> : ""}
       </div>
     </div>
   );
