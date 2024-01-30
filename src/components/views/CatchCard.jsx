662aab49b7f011e41a789a0502e5f43e1551d1e2
dkwelcher <dkwelcher@gmail.com>
2024-01-30 12:01:10 -0500

    Restructured data format in UI. Added buttons for managing catches

--- src/components/views/CatchCard.jsx
+++ src/components/views/CatchCard.jsx
@@ -1,37 +1,66 @@
 function CatchCard({ catches }) {
   return (
     <div>
-      {catches.map((catchItem, i) => (
-        <div
-          className={`${
-            i % 2 == 0 ? "bg-slate-200" : "bg-slate-300"
-          } mb-2 p-2 rounded-md`}
-          key={i}
-        >
-          <div className="grid grid-cols-9 border-b border-b-black mb-2 font-title font-medium">
-            <p>Time</p>
-            <p>Species</p>
-            <p>Latitude</p>
-            <p>Longitude</p>
-            <p>Bait</p>
-            <p>Weather</p>
-            <p>Air Temp</p>
-            <p>Water Temp</p>
-            <p>Wind Speed</p>
+      <div className="mb-4">
+        <button className="bg-white text-slate-600 px-6 py-2 rounded-sm font-paragraph font-bold">
+          Add a New Catch
+        </button>
+      </div>
+      <div>
+        {catches.map((catchItem, i) => (
+          <div
+            className={`flex justify-around items-center ${
+              i % 2 == 0 ? "bg-slate-200" : "bg-slate-300"
+            } mb-2 p-2 rounded-md`}
+            key={i}
+          >
+            <div className="flex">
+              <div className="font-title font-medium">
+                <p>Time:</p>
+                <p>Species:</p>
+                <p>Bait:</p>
+              </div>
+              <div className="ml-4 font-paragraph text-sm">
+                <p>{catchItem.time}</p>
+                <p>{catchItem.fish}</p>
+                <p>{catchItem.bait}</p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="font-title font-medium">
+                <p>Latitude:</p>
+                <p>Longitude:</p>
+                <p>Weather:</p>
+              </div>
+              <div className="ml-4 font-paragraph text-sm">
+                <p>{catchItem.latitude}</p>
+                <p>{catchItem.longitude}</p>
+                <p>{catchItem.weather}</p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="font-title font-medium">
+                <p>Air Temp:</p>
+                <p>Water Temp:</p>
+                <p>Wind Speed:</p>
+              </div>
+              <div className="ml-4 font-paragraph text-sm">
+                <p>{catchItem.airTemp}</p>
+                <p>{catchItem.waterTemp}</p>
+                <p>{catchItem.windSpeed}</p>
+              </div>
+            </div>
+            <div className="flex font-paragraph">
+              <button className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm">
+                Edit
+              </button>
+              <button className="ml-4 bg-slate-800 text-slate-200 px-6 py-2 rounded-sm">
+                Delete
+              </button>
+            </div>
           </div>
-          <div className="grid grid-cols-9 font-paragraph text-sm">
-            <p>{catchItem.time}</p>
-            <p>{catchItem.fish}</p>
-            <p>{catchItem.latitude}</p>
-            <p>{catchItem.longitude}</p>
-            <p>{catchItem.bait}</p>
-            <p>{catchItem.weather}</p>
-            <p>{catchItem.airTemp}</p>
-            <p>{catchItem.waterTemp}</p>
-            <p>{catchItem.windSpeed}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
