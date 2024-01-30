5c4f4788795075709e74f2da96824eed08f273c7
dkwelcher <dkwelcher@gmail.com>
2024-01-30 11:59:51 -0500

    Added button to allow user to select date to query database

--- src/components/DateSearch.jsx
+++ src/components/DateSearch.jsx
@@ -10,25 +10,35 @@ function DateSearch({ getTripInfo }) {
     const month = (startDate.getMonth() + 1).toString().padStart(2, "0");
     const day = startDate.getDate().toString().padStart(2, "0");
 
-    const hours = startDate.getHours().toString().padStart(2, "0");
-    const minutes = startDate.getMinutes().toString().padStart(2, "0");
+    // const hours = startDate.getHours().toString().padStart(2, "0");
+    // const minutes = startDate.getMinutes().toString().padStart(2, "0");
     // const seconds = startDate.getSeconds().toString().padStart(2, "0");
 
     const formattedDate = `${year}-${month}-${day}`;
-    const formattedTime = `${hours}:${minutes}`;
+    // const formattedTime = `${hours}:${minutes}`;
 
-    getTripInfo({ date: formattedDate, time: formattedTime });
+    getTripInfo({ date: formattedDate });
   };
 
   return (
-    <div className="relative">
-      <DatePicker
-        className="ml-1 border border-gray-300 rounded-sm text-slate-600"
-        showIcon
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        icon={<HiOutlineCalendar fontSize={20} className="text-gray-400" />}
-      />
+    <div className="flex items-center">
+      <div className="relative">
+        <DatePicker
+          className="ml-1 border border-gray-300 rounded-sm text-slate-600"
+          showIcon
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          icon={<HiOutlineCalendar fontSize={20} className="text-gray-400" />}
+        />
+      </div>
+      <div>
+        <button
+          className="ml-4 px-4 py-1 bg-white text-slate-600 rounded-sm"
+          onClick={formatSearchClick}
+        >
+          Select Date
+        </button>
+      </div>
     </div>
   );
 }
