6b5a7bc205c70ee9bd82e5dcd91655b20cafac4f
dkwelcher <dkwelcher@gmail.com>
2024-02-01 11:58:27 -0500

    Added add catch functionality

--- src/components/modals/AddCatchModal.jsx
+++ src/components/modals/AddCatchModal.jsx
@@ -1,5 +1,28 @@
-function AddCatchModal({ open, set }) {
-  if (!open) return null;
+function AddCatchModal({
+  openAddCatchModal,
+  setOpenAddCatchModal,
+  tempCatch,
+  setTempCatch,
+  catches,
+  setCatches,
+}) {
+  if (!openAddCatchModal) return null;
+
+  function handlesNewCatch() {
+    const newCatch = {
+      id: null,
+      fish: tempCatch.fish,
+      time: tempCatch.time,
+      latitude: tempCatch.latitude,
+      longitude: tempCatch.longitude,
+      bait: tempCatch.bait,
+      weather: tempCatch.weather,
+      airTemp: tempCatch.airTemp,
+      waterTemp: tempCatch.waterTemp,
+      windSpeed: tempCatch.windSpeed,
+    };
+    catches[catches.length] = newCatch;
+  }
 
   return (
     <div className="w-full h-screen fixed flex justify-center items-center bg-transparent-shadow z-50">
@@ -14,6 +37,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, time: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -23,6 +49,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, fish: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -32,6 +61,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, bait: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -41,6 +73,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, latitude: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -50,6 +85,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, longitude: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -59,6 +97,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, weather: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -68,6 +109,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, airTemp: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -77,6 +121,9 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, waterTemp: e.target.value })
+                }
               />
             </div>
             <div className="flex justify-between">
@@ -86,16 +133,25 @@ function AddCatchModal({ open, set }) {
               <input
                 className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
                 type="text"
+                onChange={(e) =>
+                  setTempCatch({ ...tempCatch, windSpeed: e.target.value })
+                }
               />
             </div>
           </form>
           <div className="flex justify-center items-center gap-4">
-            <button className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm">
+            <button
+              className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              onClick={() => {
+                handlesNewCatch();
+                setOpenAddCatchModal(false);
+              }}
+            >
               Add
             </button>
             <button
               className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
-              onClick={() => set(false)}
+              onClick={() => setOpenAddCatchModal(false)}
             >
               Cancel
             </button>
