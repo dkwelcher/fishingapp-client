0c9f47c745872206b7a47ee388ec0a5458b34e5c
dkwelcher <dkwelcher@gmail.com>
2024-02-02 11:27:19 -0500

    Added delete trip button functionality & deleted all conditional rendering
    Conditional rendering moved to ManageTrips. Now entire TripCard component will only render if there is a selected trip. Eliminates redundancy & simplifies the logic.

--- src/components/views/TripCard.jsx
+++ src/components/views/TripCard.jsx
@@ -16,24 +16,15 @@ function TripCard({
   setOpenDeleteCatchModal,
   openEditTripModal,
   setOpenEditTripModal,
+  openDeleteTripModal,
+  setOpenDeleteTripModal,
 }) {
   function handleEditTrip() {
     setTempTrip(trip);
   }
 
   return (
-    <div
-      className={`w-[95%] mx-auto mb-4 p-4 rounded-md ${
-        trip.location && trip.date
-          ? "bg-gradient-to-b from-slate-800 to-slate-400"
-          : ""
-      }`}
-    >
-      <div
-        className={`w-[500px] h-[500px] bg-green-200 m-auto ${
-          trip.location && trip.date ? "hidden" : ""
-        }`}
-      ></div>
+    <div className="w-[95%] mx-auto mb-4 p-4 rounded-md bg-gradient-to-b from-slate-800 to-slate-400">
       <div className="mb-8 flex justify-between">
         <div className="flex text-white">
           {trip.location && (
@@ -45,11 +36,7 @@ function TripCard({
             <h2 className="font-title ml-6 text-2xl">{trip.date}</h2>
           )}
         </div>
-        <div
-          className={`flex items-center ${
-            trip.location && trip.date ? "" : "hidden"
-          }`}
-        >
+        <div className="flex items-center">
           <button
             className="bg-white text-slate-600 px-6 py-2 rounded-sm font-paragraph font-bold"
             onClick={() => {
@@ -59,7 +46,12 @@ function TripCard({
           >
             Edit Current Trip Info
           </button>
-          <button className="ml-4 bg-white text-slate-600 px-6 py-2 rounded-sm font-paragraph font-bold">
+          <button
+            className="ml-4 bg-white text-slate-600 px-6 py-2 rounded-sm font-paragraph font-bold"
+            onClick={() => {
+              setOpenDeleteTripModal(true);
+            }}
+          >
             Delete Current Trip
           </button>
         </div>
