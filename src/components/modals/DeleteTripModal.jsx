dd6155a141c0a326576ea245bf084017e3ada23d
dkwelcher <dkwelcher@gmail.com>
2024-02-02 11:29:19 -0500

    Added modal for deleting trips

---
+++ src/components/modals/DeleteTripModal.jsx
@@ -0,0 +1,48 @@
+function DeleteTripModal({
+  openDeleteTripModal,
+  setOpenDeleteTripModal,
+  trip,
+  setTrip,
+}) {
+  if (!openDeleteTripModal) return null;
+
+  function handleDeleteTrip() {
+    setTrip(null);
+  }
+
+  return (
+    <div className="w-full h-screen fixed flex justify-center items-center bg-transparent-shadow z-50">
+      <div className="-translate-x-32 bg-white rounded-md font-paragraph">
+        <div className="px-20 py-8">
+          <h2 className="mb-4 font-title text-3xl">
+            Are you sure you want to delete?
+          </h2>
+          <div className="mb-4">
+            <p className="text-center">
+              {trip.location} on {trip.date}
+            </p>
+          </div>
+          <div className="flex justify-center align-items">
+            <button
+              className="mr-4 bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              onClick={() => {
+                handleDeleteTrip();
+                setOpenDeleteTripModal(false);
+              }}
+            >
+              Yes
+            </button>
+            <button
+              className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              onClick={() => setOpenDeleteTripModal(false)}
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteTripModal;
