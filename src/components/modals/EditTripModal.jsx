40c3d20d24f3b887d581d4b56a3c23f38b95910d
dkwelcher <dkwelcher@gmail.com>
2024-02-02 11:00:44 -0500

    Added modal for edit trip button click

---
+++ src/components/modals/EditTripModal.jsx
@@ -0,0 +1,65 @@
+function EditTripModal({
+  openEditTripModal,
+  setOpenEditTripModal,
+  trip,
+  setTrip,
+  tempTrip,
+  setTempTrip,
+}) {
+  if (!openEditTripModal) return null;
+
+  function handleEditTrip() {
+    setTrip(tempTrip);
+  }
+
+  return (
+    <div className="w-full h-screen fixed flex justify-center items-center bg-transparent-shadow z-50">
+      <div className="-translate-x-32 bg-white rounded-md font-paragraph">
+        <div className="px-20 py-8">
+          <h2 className="mb-4 font-title text-3xl">Edit Current Trip</h2>
+          <form action="">
+            <div className="flex flex-col">
+              <label htmlFor="">Location:</label>
+              <input
+                className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
+                type="text"
+                value={tempTrip.location}
+                onChange={(e) =>
+                  setTempTrip({ ...tempTrip, location: e.target.value })
+                }
+              />
+              <label htmlFor="">Date</label>
+              <input
+                className="mb-4 px-2 py-1 border border-solid border-zinc-400 rounded-sm"
+                type="text"
+                value={tempTrip.date}
+                onChange={(e) =>
+                  setTempTrip({ ...tempTrip, date: e.target.value })
+                }
+              />
+            </div>
+          </form>
+          <div className="flex justify-center items-center gap-4">
+            <button
+              className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              onClick={() => {
+                handleEditTrip();
+                setOpenEditTripModal(false);
+              }}
+            >
+              Edit
+            </button>
+            <button
+              className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              onClick={() => setOpenEditTripModal(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditTripModal;
