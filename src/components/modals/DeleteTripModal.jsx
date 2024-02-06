63f860d08e1981f0ae7ea147a8780d7bdc2f3237
dkwelcher <dkwelcher@gmail.com>
2024-02-06 17:47:42 -0500

    Added hover styling for buttons & modified h2 font weight

--- src/components/modals/DeleteTripModal.jsx
+++ src/components/modals/DeleteTripModal.jsx
@@ -14,7 +14,7 @@ function DeleteTripModal({
     <div className="w-full h-screen fixed flex justify-center items-center bg-transparent-shadow z-50">
       <div className="-translate-x-32 bg-white rounded-md font-paragraph">
         <div className="px-20 py-8">
-          <h2 className="mb-4 font-title text-3xl">
+          <h2 className="mb-4 font-title text-3xl font-semibold">
             Are you sure you want to delete?
           </h2>
           <div className="mb-4">
@@ -24,7 +24,7 @@ function DeleteTripModal({
           </div>
           <div className="flex justify-center align-items">
             <button
-              className="mr-4 bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              className="mr-4 bg-slate-800 text-slate-200 px-6 py-2 rounded-sm hover:bg-slate-700"
               onClick={() => {
                 handleDeleteTrip();
                 setOpenDeleteTripModal(false);
@@ -33,7 +33,7 @@ function DeleteTripModal({
               Yes
             </button>
             <button
-              className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm"
+              className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm hover:bg-slate-700"
               onClick={() => setOpenDeleteTripModal(false)}
             >
               No
