e62baa53b438fc780fb7ec8497efcfc0df13d0c5
dkwelcher <dkwelcher@gmail.com>
2024-02-06 17:51:09 -0500

    Added input validation for trips & adjusted function signatures

--- src/lib/utilities/InputValidation.jsx
+++ src/lib/utilities/InputValidation.jsx
@@ -1,4 +1,5 @@
-export function handlesCatchInputValidation(catchItem) {
+/* Main Catch Input Validation function */
+export function handleCatchInputValidation(catchItem) {
   const errors = [];
 
   const isTimeInputValid = handleTimeInputValidation(catchItem.time);
@@ -39,7 +40,21 @@ export function handlesCatchInputValidation(catchItem) {
   return errors;
 }
 
-export function handleTimeInputValidation(timeString) {
+/* Main Trip Input Validation Function */
+export function handleTripInputValidation(trip) {
+  const errors = [];
+
+  const isLocationInputValid = handleLocationInputValidation(trip.location);
+  if (!isLocationInputValid) errors.push("Location is not valid");
+
+  const isDateInputValid = handleDateInputValidation(trip.date);
+  if (!isDateInputValid) errors.push("Date is not valid");
+
+  return errors;
+}
+
+/* Helper Catch Input Validation Functions */
+function handleTimeInputValidation(timeString) {
   if (timeString === null || timeString === undefined) {
     return false;
   }
@@ -59,7 +74,7 @@ export function handleTimeInputValidation(timeString) {
   return hours >= 0 && hours <= 24 && minutes >= 0 && minutes <= 59;
 }
 
-export function handleFishInputValidation(fishString) {
+function handleFishInputValidation(fishString) {
   if (fishString === null || fishString === undefined) {
     return false;
   }
@@ -71,7 +86,7 @@ export function handleFishInputValidation(fishString) {
   return fishString.length <= 50;
 }
 
-export function handleBaitInputValidation(baitString) {
+function handleBaitInputValidation(baitString) {
   if (baitString === null || baitString === undefined) {
     return false;
   }
@@ -83,7 +98,7 @@ export function handleBaitInputValidation(baitString) {
   return baitString.length <= 50;
 }
 
-export function handleLatitudeInputValidation(latitudeString) {
+function handleLatitudeInputValidation(latitudeString) {
   if (latitudeString === null || latitudeString === undefined) {
     return false;
   }
@@ -97,7 +112,7 @@ export function handleLatitudeInputValidation(latitudeString) {
   return latitudeNumber >= -90.0 && latitudeNumber <= 90.0;
 }
 
-export function handleLongitudeInputValidation(longitudeString) {
+function handleLongitudeInputValidation(longitudeString) {
   if (longitudeString === null || longitudeString === undefined) {
     return false;
   }
@@ -111,7 +126,7 @@ export function handleLongitudeInputValidation(longitudeString) {
   return longitudeNumber >= -180.0 && longitudeNumber <= 180.0;
 }
 
-export function handleWeatherInputValidation(weatherString) {
+function handleWeatherInputValidation(weatherString) {
   if (weatherString === null || weatherString === undefined) {
     return false;
   }
@@ -123,7 +138,7 @@ export function handleWeatherInputValidation(weatherString) {
   return weatherString.length <= 25;
 }
 
-export function handleAirTempInputValidation(airTempString) {
+function handleAirTempInputValidation(airTempString) {
   if (airTempString === null || airTempString === undefined) {
     return false;
   }
@@ -137,7 +152,7 @@ export function handleAirTempInputValidation(airTempString) {
   return airTempNumber >= -50 && airTempNumber <= 150;
 }
 
-export function handleWaterTempInputValidation(waterTempString) {
+function handleWaterTempInputValidation(waterTempString) {
   if (waterTempString === null || waterTempString === undefined) {
     return false;
   }
@@ -151,7 +166,7 @@ export function handleWaterTempInputValidation(waterTempString) {
   return waterTempNumber >= -50 && waterTempNumber <= 150;
 }
 
-export function handleWindSpeedInputValidation(windSpeedString) {
+function handleWindSpeedInputValidation(windSpeedString) {
   if (windSpeedString === null || windSpeedString === undefined) {
     return false;
   }
@@ -163,4 +178,38 @@ export function handleWindSpeedInputValidation(windSpeedString) {
   const windSpeedNumber = Number(windSpeedString);
 
   return windSpeedNumber >= 0 && windSpeedNumber <= 100;
+}
+
+/* Helper Trip Input Functions */
+function handleLocationInputValidation(location) {
+  if (location === null || location === undefined) {
+    return false;
+  }
+
+  if (location.trim() === "") {
+    return false;
+  }
+
+  return location.length <= 50;
+}
+
+function handleDateInputValidation(date) {
+  if (date === null || date === undefined) {
+    return false;
+  }
+
+  if (date.trim() === "") {
+    return false;
+  }
+
+  const [year, month, day] = date.split("-").map(Number);
+
+  return (
+    year >= 0 &&
+    year <= 3000 &&
+    month >= 1 &&
+    month <= 12 &&
+    day >= 1 &&
+    day <= 31
+  );
 }
