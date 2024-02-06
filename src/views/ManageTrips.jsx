7c7d39847289dec6f03b84b2045fa5b7e5f8f10e
dkwelcher <dkwelcher@gmail.com>
2024-02-06 10:45:42 -0500

    Modified backgroundImage tailwindcss style
    Tailwind config changed bg image name to align with new dashboard view setup.

--- src/views/ManageTrips.jsx
+++ src/views/ManageTrips.jsx
@@ -240,7 +240,7 @@ function ManageTrips() {
       <div className="flex flex-col justify-center items-left text-slate-800">
         <InfoSection
           getTripInfo={getTripInfo}
-          backgroundImage="bg-viewtrip-image"
+          backgroundImage="bg-managetrips-image"
           title="Manage Your Fishing Trips"
           description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint tempora
             doloremque dolore nam, laborum maiores cupiditate inventore natus
