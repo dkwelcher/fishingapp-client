10b2153935ce4af33d2f19a4accd11ed8535f458
dkwelcher <dkwelcher@gmail.com>
2024-02-09 11:51:51 -0500

    Passed ability to open Select Date modal

--- src/components/views/InfoSection.jsx
+++ src/components/views/InfoSection.jsx
@@ -1,6 +1,12 @@
 import DateSearch from "../DateSearch";
 
-function InfoSection({ getTripInfo, backgroundImage, title, description }) {
+function InfoSection({
+  setOpenSelectDateModal,
+  getTripInfo,
+  backgroundImage,
+  title,
+  description,
+}) {
   return (
     <div
       className={`flex flex-col justify-center items-left px-10 py-20 mb-8 bg-cover bg-center text-white ${backgroundImage}`}
@@ -8,7 +14,10 @@ function InfoSection({ getTripInfo, backgroundImage, title, description }) {
       <h1 className="font-title font-medium text-4xl mb-4">{title}</h1>
       <p className="w-[40%] font-paragraph text-md mb-8">{description}</p>
       <div>
-        <DateSearch getTripInfo={getTripInfo} />
+        <DateSearch
+          setOpenSelectDateModal={setOpenSelectDateModal}
+          getTripInfo={getTripInfo}
+        />
       </div>
     </div>
   );
