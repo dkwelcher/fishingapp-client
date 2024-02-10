7b300a3dfddd274a5267be877ae6db8503baca20
dkwelcher <dkwelcher@gmail.com>
2024-02-10 13:54:20 -0500

    Removed unused comments & modified data type passed into getTripInfo

--- src/components/DateSearch.jsx
+++ src/components/DateSearch.jsx
@@ -10,14 +10,9 @@ function DateSearch({ setOpenSelectDateModal, getTripInfo }) {
     const month = (startDate.getMonth() + 1).toString().padStart(2, "0");
     const day = startDate.getDate().toString().padStart(2, "0");
 
-    // const hours = startDate.getHours().toString().padStart(2, "0");
-    // const minutes = startDate.getMinutes().toString().padStart(2, "0");
-    // const seconds = startDate.getSeconds().toString().padStart(2, "0");
-
     const formattedDate = `${year}-${month}-${day}`;
-    // const formattedTime = `${hours}:${minutes}`;
 
-    getTripInfo({ date: formattedDate });
+    getTripInfo(formattedDate);
   };
 
   return (
