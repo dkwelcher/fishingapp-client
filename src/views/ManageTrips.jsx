fce88e00810cffad4498cb9a4ad3119a59b1419e
dkwelcher <dkwelcher@gmail.com>
2024-02-10 13:55:56 -0500

    Added useEffect hooks for selecting a date & selecting existing trips to display catches

--- src/views/ManageTrips.jsx
+++ src/views/ManageTrips.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InfoSection from "../components/views/InfoSection";
 import TripCard from "../components/views/TripCard";
 import SelectDateModal from "../components/modals/SelectDateModal";
@@ -11,8 +11,8 @@ import Logo from "../assets/logo.png";
 
 function ManageTrips() {
   const [user, setUser] = useState({
-    id: "1",
-    username: "user",
+    id: 1302,
+    username: "newUser",
   });
 
   const [openSelectDateModal, setOpenSelectDateModal] = useState(false);
@@ -25,180 +25,72 @@ function ManageTrips() {
   const [tempCatch, setTempCatch] = useState({});
   const [tempTrip, setTempTrip] = useState({});
 
-  const [tripInfo, setTripInfo] = useState({ date: "", time: "" });
+  const [tripInfo, setTripInfo] = useState();
 
-  const getTripInfo = (tripInfo) => {
-    // query the database
-    // update the useState for trip
+  function getTripInfo(tripInfoItem) {
+    setTripInfo(tripInfoItem);
+    //setTripInfo("2024-02-02");
+  }
 
-    setTripInfo(tripInfo);
-  };
+  const [trips, setTrips] = useState([]);
 
-  const [trips, setTrips] = useState([
-    {
-      id: "1",
-      location: "Clarks Hill",
-      date: "2024-01-24",
-    },
-    {
-      id: "2",
-      location: "Lake Murray",
-      date: "2024-02-01",
-    },
-  ]);
+  const [trip, setTrip] = useState({});
 
-  const [trip, setTrip] = useState({
-    id: "1",
-    location: "Clarks Hill",
-    date: "2024-01-24",
-  });
+  const [catches, setCatches] = useState([]);
 
-  const [catches, setCatches] = useState([
-    {
-      id: "1",
-      fish: "Striped Bass",
-      time: "05:30",
-      latitude: "33.691743",
-      longitude: "-82.217761",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "65",
-      waterTemp: "60",
-      windSpeed: "8",
-    },
-    {
-      id: "2",
-      fish: "Striped Bass",
-      time: "05:36",
-      latitude: "32.259143",
-      longitude: "-81.198345",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "65",
-      waterTemp: "60",
-      windSpeed: "8",
-    },
-    {
-      id: "3",
-      fish: "Striped Bass",
-      time: "05:42",
-      latitude: "33.613486",
-      longitude: "-82.991645",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "65",
-      waterTemp: "60",
-      windSpeed: "8",
-    },
-    {
-      id: "4",
-      fish: "Channel Catfish",
-      time: "05:44",
-      latitude: "33.613589",
-      longitude: "-82.992346",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "65",
-      waterTemp: "60",
-      windSpeed: "8",
-    },
-    {
-      id: "5",
-      fish: "Striped Bass",
-      time: "06:45",
-      latitude: "34.513486",
-      longitude: "-83.543168",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "68",
-      waterTemp: "60",
-      windSpeed: "8",
-    },
-    {
-      id: "7",
-      fish: "Largemouth Bass",
-      time: "06:47",
-      latitude: "34.513246",
-      longitude: "-83.549476",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "68",
-      waterTemp: "60",
-      windSpeed: "8",
-    },
-    {
-      id: "8",
-      fish: "Striped Bass",
-      time: "08:26",
-      latitude: "36.761389",
-      longitude: "-86.498315",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "72",
-      waterTemp: "61",
-      windSpeed: "8",
-    },
-    {
-      id: "9",
-      fish: "Channel Catfish",
-      time: "08:28",
-      latitude: "36.761389",
-      longitude: "-86.498315",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "72",
-      waterTemp: "61",
-      windSpeed: "8",
-    },
-    {
-      id: "10",
-      fish: "Striped Bass",
-      time: "08:29",
-      latitude: "35.984351",
-      longitude: "-86.986479",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "72",
-      waterTemp: "61",
-      windSpeed: "8",
-    },
-    {
-      id: "11",
-      fish: "Striped Bass",
-      time: "08:42",
-      latitude: "36.846388",
-      longitude: "-86.912549",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "74",
-      waterTemp: "61",
-      windSpeed: "8",
-    },
-    {
-      id: "12",
-      fish: "Striped Bass",
-      time: "08:53",
-      latitude: "36.761389",
-      longitude: "-86.498315",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "74",
-      waterTemp: "61",
-      windSpeed: "8",
-    },
-    {
-      id: "13",
-      fish: "Striped Bass",
-      time: "08:59",
-      latitude: "36.761389",
-      longitude: "-86.998315",
-      bait: "Blueback Herring",
-      weather: "cloudy",
-      airTemp: "74",
-      waterTemp: "61",
-      windSpeed: "8",
-    },
-  ]);
+  const GET_TRIPS_BY_ID = `http://localhost:8080/trips&date?id=${user.id}&date=${tripInfo}`;
+  const [fetchTripsError, setFetchTripsError] = useState();
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      if (!tripInfo) return;
+
+      try {
+        const response = await fetch(GET_TRIPS_BY_ID);
+        const tripObjects = await response.json();
+        setTrips(tripObjects);
+      } catch (error) {
+        setFetchTripsError(error);
+        console.log(fetchTripsError);
+      }
+    };
+
+    fetchTrips();
+  }, [tripInfo, user.id]);
+
+  const GET_CATCHES_BY_TRIP_ID = `http://localhost:8080/catches?tripId=${trip.id}`;
+  const [fetchCatchesError, setFetchCatchesError] = useState();
+
+  useEffect(() => {
+    const fetchCatches = async () => {
+      if (!trip || !trip.id) return;
+
+      try {
+        const response = await fetch(GET_CATCHES_BY_TRIP_ID);
+        const catchObjectArray = await response.json();
+
+        const convertedCatches = catchObjectArray.map((catchObject) => ({
+          id: catchObject.catchId,
+          time: catchObject.time.slice(0, 5),
+          latitude: catchObject.latitude,
+          longitude: catchObject.longitude,
+          fish: catchObject.species,
+          bait: catchObject.lureOrBait,
+          weather: catchObject.weatherCondition,
+          airTemp: catchObject.airTemperature,
+          waterTemp: catchObject.waterTemperature,
+          windSpeed: catchObject.windSpeed,
+        }));
+
+        setCatches(convertedCatches);
+      } catch (error) {
+        setFetchCatchesError(error);
+        console.log(fetchCatchesError);
+      }
+    };
+
+    fetchCatches();
+  }, [trip]);
 
   return (
     <div>
