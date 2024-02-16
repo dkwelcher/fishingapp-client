7fced97b2d7dac9aa957a220932b29d59ae6a5b4
dkwelcher <dkwelcher@gmail.com>
2024-02-16 08:58:24 -0500

    Commented faq link

--- src/lib/constants/index.jsx
+++ src/lib/constants/index.jsx
@@ -17,10 +17,10 @@ export const DASHBOARD_SIDEBAR_LINKS = [
 ];
 
 export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
-  {
-    key: "faq",
-    label: "FAQ",
-    path: "/faq",
-    icon: <HiOutlineQuestionMarkCircle />,
-  },
+  // {
+  //   key: "faq",
+  //   label: "FAQ",
+  //   path: "/faq",
+  //   icon: <HiOutlineQuestionMarkCircle />,
+  // },
 ];
