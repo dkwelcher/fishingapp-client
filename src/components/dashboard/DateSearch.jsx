1fc72ce6f39c7139a45bedf9e104c8c025f48cb8
dkwelcher <dkwelcher@gmail.com>
2024-02-19 16:14:07 -0500

    Moved to new directory within components: dashboard

---
+++ src/components/dashboard/DateSearch.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import { HiOutlineCalendar } from "react-icons/hi";
+
+function DateSearch({ setOpenSelectDateModal, getTripDate }) {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const handleSearchClick = () => {
+    const year = startDate.getFullYear();
+    const month = (startDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = startDate.getDate().toString().padStart(2, "0");
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    getTripDate(formattedDate);
+  };
+
+  return (
+    <div className="flex items-center">
+      <div className="relative">
+        <DatePicker
+          className="ml-1 border border-gray-300 rounded-sm text-slate-600"
+          showIcon
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          icon={<HiOutlineCalendar fontSize={20} className="text-gray-400" />}
+        />
+      </div>
+      <div>
+        <button
+          className="ml-4 px-4 py-1 bg-white text-slate-600 rounded-sm"
+          onClick={() => {
+            setOpenSelectDateModal(true);
+            handleSearchClick();
+          }}
+        >
+          Select Date
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DateSearch;
---
+++ src/components/dashboard/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+function Layout({ setUser }) {
+  return (
+    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+      <Sidebar setUser={setUser} />
+      <div className="flex-col flex-1 overflow-auto">
+        <div>{<Outlet />}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
