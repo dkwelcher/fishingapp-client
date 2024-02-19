5f4acd88d52d642f90e2e606db5d6385f0b47fd2
dkwelcher <dkwelcher@gmail.com>
2024-02-19 09:53:28 -0500

    Added redirect function & localStorage check for user
    Redirect function: redirects unauthorized users from accessing url paths (dashboard). localStorage check: Checks for user in case they refresh browser.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Layout from "./components/shared/Layout";
 import Home from "./views/Home";
 import ManageTrips from "./views/ManageTrips";
@@ -6,17 +12,36 @@ import LandingPage from "./views/LandingPage";
 import Signup from "./views/Signup";
 import Login from "./views/Login";
 
+const PrivateView = ({ user, children }) => {
+  return user.id ? children : <Navigate to="/login" replace />;
+};
+
 function App() {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : {};
+  });
+
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="manage-trips" element={<ManageTrips />} />
+          <Route
+            path="/login"
+            element={<Login user={user} setUser={setUser} />}
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateView user={user}>
+                <Layout setUser={setUser} />
+              </PrivateView>
+            }
+          >
+            <Route index element={<Home user={user} />} />
+            <Route path="manage-trips" element={<ManageTrips user={user} />} />
           </Route>
         </Routes>
       </Router>
