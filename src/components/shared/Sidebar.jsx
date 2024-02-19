0cb2fbfc757a1b6db63de7099cf43bf4254380b7
dkwelcher <dkwelcher@gmail.com>
2024-02-19 10:00:56 -0500

    Pass setUser prop. Remove localStorage items on logout

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -10,10 +10,13 @@ import { HiOutlineLogout } from "react-icons/hi";
 const linkClasses =
   "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
 
-function Sidebar() {
+function Sidebar({ setUser }) {
   const navigate = useNavigate();
 
   function handleLogout() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("authToken");
+    setUser({});
     navigate("/");
   }
 
