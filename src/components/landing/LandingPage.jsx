61c640960847e41b2836f3b7246d603adfd4469a
dkwelcher <dkwelcher@gmail.com>
2024-02-21 08:58:20 -0500

    Improved layout & responsiveness for all devices. Added hamburger menu for mobile / tablet

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,43 +1,60 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import LargemouthBass from "../../assets/largemouth-bass.png";
 import ChannelCatfish from "../../assets/channel-catfish.png";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
 function LandingPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleNavbar() {
+    setIsOpen(!isOpen);
+  }
+
   /* Tailwind Class Styles */
   const pageStyles = "font-paragraph";
   const heroAndNavigationContainerStyles =
     "h-[80vh] bg-hero-image bg-cover bg-center font-medium";
-  const navHeaderStyles = "flex justify-between pt-2 px-2";
+  const navHeaderStyles = "flex justify-between pt-2 px-2 lg:pt-4 lg:px-4";
   const logoContainerStyles = "flex justify-center items-center";
-  const logoImageStyles = "size-9";
+  const logoImageStyles = "size-9 md:size-10 lg:size-14";
   const logoLinkStyles = "hover:no-underline";
-  const logoNameStyles = "text-3xl pl-2 text-white font-cursive font-black";
-  const headerLinkContainerStyles = "flex items-center invisible";
-  const headerLinkHamburgerStyles = "absolute right-4 text-3xl text-white";
+  const logoNameStyles =
+    "text-3xl pl-2 text-white font-cursive font-black md:text-4xl lg:text-5xl";
+  const headerLinkContainerStyles =
+    "flex items-center invisible md:visible md:gap-4 md: pr-4";
+  const headerButtonHamburgerStyles = "";
+  const headerLinkHamburgerContainerStyles =
+    "flex flex-col justify-center items-center absolute bg-white w-[90%] h-1/4 translate-x-3";
+  const headerLinkHamburgerStyles =
+    "hover:no-underline text-black text-lg hover:text-blue-700";
   const headerLinkStyles =
-    "hover:no-underline text-white text-lg hover:text-blue-700";
+    "hover:no-underline text-white text-lg hover:text-blue-700 lg:text-2xl";
   const heroContentContainerStyles =
     "flex flex-col justify-center items-center h-4/5";
   const heroTitleStyles =
-    "text-center font-title font-bold text-5xl leading-normal text-white text-shadow";
+    "text-center font-title font-bold text-5xl leading-normal text-white text-shadow md:text-6xl lg:w-3/6 lg:text-7xl xl:text-8xl";
   const heroGetStartedLinkStyles =
     "px-6 py-4 border border-solid border-white rounded-2xl hover:no-underline text-white text-2xl hover:bg-white hover:text-gray-900";
-  const mainSectionsContainer1Styles = "pt-10 px-[10%] flex flex-col";
+  const mainSectionsContainer1Styles =
+    "pt-10 px-[10%] flex flex-col sm:items-center lg:grid lg:grid-cols-2 lg:pt-20 lg:justify-items-start xl:px-[20%]";
   const mainSectionsContainer2Styles =
-    "pt-10 pb-0 px-[10%] flex flex-col-reverse";
-  const mainSectionsImageStyles = "rounded-2xl";
-  const imageLeftTextRightSectionContainerStyles = "";
-  const imageRightTextLeftSectionContainerStyles = "";
+    "pt-10 px-[10%] flex flex-col-reverse sm:items-center lg:grid lg:grid-cols-2 lg:pt-20 lg:justify-items-end xl:px-[20%]";
+  const mainSectionsImageStyles = "pb-4 rounded-2xl sm:w-[500px] lg:pb-0";
+  const imageLeftTextRightSectionContainerStyles =
+    "sm:w-[500px] lg:py-4 lg:px-10";
+  const imageRightTextLeftSectionContainerStyles =
+    "sm:w-[500px] lg:py-4 lg:px-10";
   const sectionTitleStyles = "text-4xl pb-4 font-semibold text-center";
   const sectionSpanStyles = "text-blue-700";
   const sectionParagraphStyles = "text-zinc-600 text-md font-medium";
   const calloutContainerStyles =
-    "mt-4 flex bg-callout-image bg-cover bg-center h-screen justify-center items-center";
+    "mt-4 flex bg-callout-image bg-cover bg-center h-screen justify-center items-center lg:mt-20";
   const calloutContentContainerStyles =
     "flex flex-col justify-center items-center";
   const calloutTitleStyles =
-    "pb-8 text-center text-3xl text-white text-shadow leading-normal font-semibold";
+    "pb-8 text-center text-3xl text-white text-shadow leading-normal font-semibold md:w-5/6 md:text-4xl lg:w-7/12 lg:text-6xl xl:text-7xl";
   const calloutLinkStyles =
     "px-4 py-4 border-4 border-solid border-white rounded-2xl hover:no-underline text-5xl font-medium text-white text-shadow bg-transparent-shadow hover:bg-blue-700";
   const footerContainerStyles =
@@ -61,8 +78,15 @@ function LandingPage() {
                 <h2 className={logoNameStyles}>Fishing App</h2>
               </a>
             </div>
-            <button className={headerLinkHamburgerStyles}>
-              {<RxHamburgerMenu />}
+            <button
+              className={`absolute right-4 text-3xl text-white z-50 md:invisible ${
+                isOpen && "text-black"
+              }`}
+              onClick={() => {
+                toggleNavbar();
+              }}
+            >
+              {isOpen ? <RxCross1 /> : <RxHamburgerMenu />}
             </button>
             <div className={headerLinkContainerStyles}>
               <a className={headerLinkStyles} href="/signup">
@@ -72,6 +96,16 @@ function LandingPage() {
                 <p>Log in</p>
               </a>
             </div>
+            {isOpen && (
+              <div className={headerLinkHamburgerContainerStyles}>
+                <a className={headerLinkHamburgerStyles} href="/signup">
+                  <p>Sign up</p>
+                </a>
+                <a className={headerLinkHamburgerStyles} href="/login">
+                  <p>Log in</p>
+                </a>
+              </div>
+            )}
           </header>
         </nav>
         <div className={heroContentContainerStyles}>
