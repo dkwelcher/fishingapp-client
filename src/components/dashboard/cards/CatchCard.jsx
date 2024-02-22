cc1612c8a928f7a54c4632a57e5e632f8cab9f02
dkwelcher <dkwelcher@gmail.com>
2024-02-22 16:29:57 -0500

    Modified styles for responsive layout

--- src/components/dashboard/cards/CatchCard.jsx
+++ src/components/dashboard/cards/CatchCard.jsx
@@ -26,15 +26,16 @@ function CatchCard({
 
   /* Tailwind Class Styles */
   const catchesContainerStyles = "font-paragraph";
-  const addCatchButtonContainerStyles = "pb-4 flex flex-col";
+  const addCatchButtonContainerStyles =
+    "pb-2 flex flex-col min-[900px]:flex-row";
   const addCatchButtonStyles =
     "bg-slate-200 text-slate-600 px-6 py-2 rounded-sm font-bold hover:bg-slate-100 hover:text-slate-500";
-
-  const catchCardColumnStyles = "px-2 grid grid-cols-2";
-  const catchCardInfoSubHeaderContainer = "font-title font-medium";
-  const catchCardInfoResultsContainer = "text-sm";
+  const catchCardContainerStyles =
+    "sm:grid sm:grid-cols-2 sm:gap-x-2 md:block min-[900px]:grid min-[900px]:grid min-[1200px]:grid-cols-3 xl:gap-x-4 2xl:grid-cols-4 2xl:gap-x-6 min-[1800px]:grid-cols-5 min-[2200px]:flex";
+  const catchCardInfoContainer = "flex min-[2200px]:flex-col";
+  const catchCardSubHeaderStyles = "font-semibold pr-2";
   const catchCardButtonContainerStyles =
-    "pt-4 flex justify-center items-center gap-4";
+    "pt-4 flex justify-center items-center gap-4 xl:pt-0";
   const catchCardButtonStyles =
     "bg-slate-800 text-slate-200 px-6 py-2 rounded-sm hover:bg-slate-700";
   /* End Tailwind Class Styles */
@@ -52,44 +53,46 @@ function CatchCard({
       <div>
         {catches.map((catchItem, i) => (
           <div
-            className={`grid grid-cols-1 ${
+            className={`my-2 p-4 xl:flex xl:justify-between xl:items-center xl:px-10 min-[2200px]:justify-around ${
               i % 2 == 0 ? "bg-slate-200" : "bg-slate-300"
-            } mb-2 p-2 rounded-md`}
+            }`}
             key={catchItem.id}
           >
-            <div className={catchCardColumnStyles}>
-              <div className={catchCardInfoSubHeaderContainer}>
-                <p>Time:</p>
-                <p>Fish:</p>
-                <p>Bait:</p>
-              </div>
-              <div className={catchCardInfoResultsContainer}>
+            <div className={catchCardContainerStyles}>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Time:</p>
                 <p>{handleTimeConversionTo12HourFormat(catchItem.time)}</p>
+              </div>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Fish:</p>
                 <p>{catchItem.fish}</p>
+              </div>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Bait:</p>
                 <p>{catchItem.bait}</p>
               </div>
-            </div>
-            <div className={catchCardColumnStyles}>
-              <div className={catchCardInfoSubHeaderContainer}>
-                <p>Latitude:</p>
-                <p>Longitude:</p>
-                <p>Weather:</p>
-              </div>
-              <div className={catchCardInfoResultsContainer}>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Latitude:</p>
                 <p>{catchItem.latitude}</p>
+              </div>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Longitude:</p>
                 <p>{catchItem.longitude}</p>
+              </div>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Weather:</p>
                 <p>{catchItem.weather}</p>
               </div>
-            </div>
-            <div className={catchCardColumnStyles}>
-              <div className={catchCardInfoSubHeaderContainer}>
-                <p>Air Temp:</p>
-                <p>Water Temp:</p>
-                <p>Wind Speed:</p>
-              </div>
-              <div className={catchCardInfoResultsContainer}>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Air Temp:</p>
                 <p>{catchItem.airTemp}</p>
+              </div>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Water Temp:</p>
                 <p>{catchItem.waterTemp}</p>
+              </div>
+              <div className={catchCardInfoContainer}>
+                <p className={catchCardSubHeaderStyles}>Wind Speed:</p>
                 <p>{catchItem.windSpeed}</p>
               </div>
             </div>
