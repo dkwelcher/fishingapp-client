492ce27def1a62e40609b8b419463a575075be0b
dkwelcher <dkwelcher@gmail.com>
2024-02-22 21:03:32 -0500

    Modified color schemes to match theme

--- src/components/dashboard/cards/CatchCard.jsx
+++ src/components/dashboard/cards/CatchCard.jsx
@@ -25,19 +25,18 @@ function CatchCard({
   }
 
   /* Tailwind Class Styles */
-  const catchesContainerStyles = "font-paragraph";
-  const addCatchButtonContainerStyles =
-    "pb-2 flex flex-col min-[900px]:flex-row";
+  const catchesContainerStyles = "font-paragraph text-slate-200";
+  const addCatchButtonContainerStyles = "pb-2 flex flex-col items-center";
   const addCatchButtonStyles =
-    "bg-slate-200 text-slate-600 px-6 py-2 rounded-sm font-bold hover:bg-slate-100 hover:text-slate-500";
-  const catchCardContainerStyles =
-    "sm:grid sm:grid-cols-2 sm:gap-x-2 md:block min-[900px]:grid min-[900px]:grid min-[1200px]:grid-cols-3 min-[1200px]:pb-2 xl:gap-x-4 xl:pb-0 2xl:grid-cols-4 2xl:gap-x-6 min-[1800px]:grid-cols-5 min-[2200px]:flex";
-  const catchCardInfoContainer = "flex min-[2200px]:flex-col";
-  const catchCardSubHeaderStyles = "font-semibold pr-2";
+    "w-[300px] bg-slate-300 text-slate-600 px-6 py-2 rounded-sm font-bold shadow-md shadow-slate-900 hover:bg-slate-100 hover:text-slate-500";
+  const catchCardContainerStyles = "";
+  const catchCardInfoContainer = "flex";
+  const catchCardSubHeaderStyles = "font-bold pr-2";
+  const catchCardResultStyles = "text-slate-300";
   const catchCardButtonContainerStyles =
-    "pt-4 flex justify-center items-center gap-4 xl:pt-0";
+    "pt-4 flex justify-center items-center gap-4";
   const catchCardButtonStyles =
-    "bg-slate-800 text-slate-200 px-6 py-2 rounded-sm hover:bg-slate-700";
+    "bg-slate-800 text-slate-200 px-6 py-2 rounded-sm hover:bg-slate-700 shadow-md shadow-slate-900";
   /* End Tailwind Class Styles */
 
   return (
@@ -50,78 +49,86 @@ function CatchCard({
           Add a New Catch
         </button>
       </div>
-      <div>
-        {catches.map((catchItem, i) => (
-          <div
-            className={`my-2 p-4 xl:flex xl:justify-between xl:items-center xl:px-10 min-[2200px]:justify-around ${
-              i % 2 == 0 ? "bg-slate-200" : "bg-slate-300"
-            }`}
-            key={catchItem.id}
-          >
-            <div className={catchCardContainerStyles}>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Time:</p>
-                <p>{handleTimeConversionTo12HourFormat(catchItem.time)}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Fish:</p>
-                <p>{catchItem.fish}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Bait:</p>
-                <p>{catchItem.bait}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Latitude:</p>
-                <p>{catchItem.latitude}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Longitude:</p>
-                <p>{catchItem.longitude}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Weather:</p>
-                <p>{catchItem.weather}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Air Temp:</p>
-                <p>{catchItem.airTemp}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Water Temp:</p>
-                <p>{catchItem.waterTemp}</p>
-              </div>
-              <div className={catchCardInfoContainer}>
-                <p className={catchCardSubHeaderStyles}>Wind Speed:</p>
-                <p>{catchItem.windSpeed}</p>
+      <div className="pt-2">
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,300px)] justify-center">
+          {catches.map((catchItem, i) => (
+            <div
+              className={`p-4 shadow-md shadow-slate-900 max-w-[300px] ${
+                i % 2 == 0
+                  ? "bg-gradient-to-b from-slate-600 to-slate-700"
+                  : "bg-gradient-to-b from-slate-500 to-slate-600"
+              }`}
+              key={catchItem.id}
+            >
+              <div>
+                <div className={catchCardContainerStyles}>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Time:</p>
+                    <p className={catchCardResultStyles}>
+                      {handleTimeConversionTo12HourFormat(catchItem.time)}
+                    </p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Fish:</p>
+                    <p>{catchItem.fish}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Bait:</p>
+                    <p>{catchItem.bait}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Latitude:</p>
+                    <p>{catchItem.latitude}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Longitude:</p>
+                    <p>{catchItem.longitude}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Weather:</p>
+                    <p>{catchItem.weather}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Air Temp:</p>
+                    <p>{catchItem.airTemp}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Water Temp:</p>
+                    <p>{catchItem.waterTemp}</p>
+                  </div>
+                  <div className={catchCardInfoContainer}>
+                    <p className={catchCardSubHeaderStyles}>Wind Speed:</p>
+                    <p>{catchItem.windSpeed}</p>
+                  </div>
+                </div>
+                <div className={catchCardButtonContainerStyles}>
+                  <button
+                    className={catchCardButtonStyles}
+                    data-key={i}
+                    onClick={(e) => {
+                      const dataKey = e.currentTarget.getAttribute("data-key");
+                      setOpenEditCatchModal(true);
+                      handleEditCatch(dataKey);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className={catchCardButtonStyles}
+                    data-key={i}
+                    onClick={(e) => {
+                      const dataKey = e.currentTarget.getAttribute("data-key");
+                      setOpenDeleteCatchModal(true);
+                      handleDeleteCatch(dataKey);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-            <div className={catchCardButtonContainerStyles}>
-              <button
-                className={catchCardButtonStyles}
-                data-key={i}
-                onClick={(e) => {
-                  const dataKey = e.currentTarget.getAttribute("data-key");
-                  setOpenEditCatchModal(true);
-                  handleEditCatch(dataKey);
-                }}
-              >
-                Edit
-              </button>
-              <button
-                className={catchCardButtonStyles}
-                data-key={i}
-                onClick={(e) => {
-                  const dataKey = e.currentTarget.getAttribute("data-key");
-                  setOpenDeleteCatchModal(true);
-                  handleDeleteCatch(dataKey);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
