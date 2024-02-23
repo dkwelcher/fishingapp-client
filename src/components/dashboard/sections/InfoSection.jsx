aa81f005967f10db58c1cd73d34b86cc4345de58
dkwelcher <dkwelcher@gmail.com>
2024-02-22 21:05:02 -0500

    Added shadow to info container

--- src/components/dashboard/sections/InfoSection.jsx
+++ src/components/dashboard/sections/InfoSection.jsx
@@ -8,11 +8,11 @@ function InfoSection({
   description,
 }) {
   /* Tailwind Class Styles */
-  const infoSectionStyles = `flex flex-col justify-center items-left px-4 pt-12 pb-4 bg-cover bg-center text-white ${backgroundImage} md:py-12 md:px-4`;
+  const infoSectionStyles = `flex flex-col justify-center items-left px-4 pt-12 pb-4 bg-cover bg-center text-white ${backgroundImage} md:py-12 md:px-4 shadow-md shadow-slate-800`;
   const infoSectionTitleStyles =
     "font-title font-medium text-3xl pb-2 lg:text-4xl";
   const infoSectionDescriptionStyles =
-    "font-paragraph text-md pb-4 lg:w-[60%]  2xl:w-[40%]";
+    "font-paragraph text-md pb-4 lg:w-[60%] 2xl:w-[40%]";
   /* End Tailwind Class Styles */
 
   return (
