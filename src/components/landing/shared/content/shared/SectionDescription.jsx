b21989bc2c0ead7dd950600eb65651903a5b3001
dkwelcher <dkwelcher@gmail.com>
2024-10-30 18:09:02 -0400

    Added component for section descriptive text

---
+++ src/components/landing/shared/content/shared/SectionDescription.jsx
@@ -0,0 +1,9 @@
+function SectionDescription({ text }) {
+  return (
+    <p className="text-slate-400 text-sm md:text-base lg:text-lg 2xl:text-xl">
+      {text}
+    </p>
+  );
+}
+
+export default SectionDescription;
