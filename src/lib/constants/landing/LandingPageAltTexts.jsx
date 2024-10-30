107109ad3f9f804ba7e5d87e2ebf44aad1c96977
dkwelcher <dkwelcher@gmail.com>
2024-10-30 18:11:05 -0400

    Added a constants module for image alt texts

---
+++ src/lib/constants/landing/LandingPageAltTexts.jsx
@@ -0,0 +1,15 @@
+// striped-bass.png
+export const STRIPED_BASS_ALT_TEXT =
+  "Caught striped bass being held up by a man";
+
+//blue-catfish.png
+export const BLUE_CATFISH_ALT_TEXT =
+  "Caught blue catfish being held up by a man";
+
+//pier.png
+export const PIER_ALT_TEXT = "Looking out from beach pier toward town";
+
+export const SUBSECTION_IMAGE_ALT_TEXTS = [
+  BLUE_CATFISH_ALT_TEXT,
+  PIER_ALT_TEXT,
+];
