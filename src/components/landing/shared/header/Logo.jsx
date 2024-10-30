ebc3c770d5aa193864c8438f0929967fc712aced
dkwelcher <dkwelcher@gmail.com>
2024-10-30 12:13:34 -0400

    Added component for app logo image & name

---
+++ src/components/landing/shared/header/Logo.jsx
@@ -0,0 +1,18 @@
+import LogoImage from "../../../../assets/logo.png";
+
+function Logo() {
+  return (
+    <div className="flex gap-x-2">
+      <img
+        className="size-8 sm:size-9 md:size-10 lg:size-14"
+        src={LogoImage}
+        alt="Largemouth bass jumping out of the water"
+      />
+      <h2 className="font-cursive text-2xl text-slate-200 sm:text-3xl md:text-4xl lg:text-5xl">
+        Fishing App
+      </h2>
+    </div>
+  );
+}
+
+export default Logo;
