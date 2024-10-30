362e2ef6fc948990f2f13f736c56fcd7fb968ae6
dkwelcher <dkwelcher@gmail.com>
2024-10-30 18:56:12 -0400

    Abstracted object array mapping & updated imports
    All constants are now handled in one module. Subsection object array mapped in the module.

--- src/components/landing/shared/content/MainContent.jsx
+++ src/components/landing/shared/content/MainContent.jsx
@@ -1,26 +1,16 @@
 import Section1Image from "../../../../assets/striped-bass.png";
-import Section2Image from "../../../../assets/blue-catfish.png";
-import Section3Image from "../../../../assets/pier.png";
-import { SUBSECTION_IMAGE_ALT_TEXTS } from "../../../../lib/constants/landing/LandingPageAltTexts.jsx";
-import { SECTION1_DESCRIPTION } from "../../../../lib/constants/landing/LandingPageText";
-import { SUBSECTION_TITLES } from "../../../../lib/constants/landing/LandingPageText";
-import { SUBSECTION_DESCRIPTIONS } from "../../../../lib/constants/landing/LandingPageText";
+import {
+  SECTION1_DESCRIPTION,
+  SUBSECTION_OBJECTS,
+} from "../../../../lib/constants/landing/MainContentConstants.jsx";
 import Section from "./section/Section.jsx";
 import MultiSection from "./section/MultiSection.jsx";
 
 function MainContent() {
-  const subsectionImages = [Section2Image, Section3Image];
-  const subsectionObjects = subsectionImages.map((image, index) => ({
-    image: image,
-    altText: SUBSECTION_IMAGE_ALT_TEXTS[index],
-    title: SUBSECTION_TITLES[index],
-    description: SUBSECTION_DESCRIPTIONS[index],
-  }));
-
   return (
     <main className="py-12 px-4 sm:py-20 md:px-12 lg:py-28 2xl:py-36 text-slate-800">
       <Section image={Section1Image} description={SECTION1_DESCRIPTION} />
-      <MultiSection objects={subsectionObjects} />
+      <MultiSection objects={SUBSECTION_OBJECTS} />
     </main>
   );
 }
