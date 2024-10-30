7e2081d686fed3883dcf1dac7df6739a63ee2717
dkwelcher <dkwelcher@gmail.com>
2024-10-30 12:12:05 -0400

    Added landing page header component
    Elements from header will be abstracted to smaller components.

---
+++ src/components/landing/shared/header/LandingPageHeader.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from "react";
+import Logo from "./Logo.jsx";
+import MobileMenu from "./MobileMenu.jsx";
+import AuthLink from "./AuthLink.jsx";
+import MobileAuthLink from "./MobileAuthLink.jsx";
+
+function LandingPageHeader({ handleLoginEntry, handleSignupEntry }) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  function toggleNavbar() {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  /* 
+  The useEffect listens for mouse clicks outside of the specified dropdown component, and it will close the dropdown if such a click is detected.
+
+  @return Cleanup function that removes mousedown event listener when component unmounts.
+  */
+  useEffect(() => {
+    const handleClickOutsideDropdown = (event) => {
+      let targetElement = event.target;
+      let isInsideDropdown = false;
+
+      while (targetElement != null) {
+        if (targetElement === dropdownRef.current) {
+          isInsideDropdown = true;
+          break;
+        }
+        targetElement = targetElement.parentNode;
+      }
+
+      if (!isInsideDropdown) {
+        setIsMobileMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutsideDropdown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideDropdown);
+    };
+  }, []);
+
+  return (
+    <header className="flex justify-between pt-2 px-2 lg:pt-4 lg:px-4">
+      <Logo />
+      <MobileMenu
+        toggleNavbar={toggleNavbar}
+        isMobileMenuOpen={isMobileMenuOpen}
+      />
+      <div className="flex items-center cursor-pointer invisible md:visible">
+        <AuthLink handleAuth={handleSignupEntry} linkText={"Sign up"} />
+        <AuthLink handleAuth={handleLoginEntry} linkText={"Log in"} />
+      </div>
+      <div
+        ref={dropdownRef}
+        className={`w-full md:w-11/12 flex flex-col justify-center items-center gap-y-2 absolute bg-white h-1/4 rounded-sm transition-all duration-1000 ease-in-out -translate-x-2 translate-y-11 ${
+          isMobileMenuOpen ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+      >
+        <MobileAuthLink handleAuth={handleSignupEntry} linkText={"Sign up"} />
+        <MobileAuthLink handleAuth={handleLoginEntry} linkText={"Log in"} />
+      </div>
+    </header>
+  );
+}
+
+export default LandingPageHeader;
