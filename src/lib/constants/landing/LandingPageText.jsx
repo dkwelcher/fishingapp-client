319df0a42d63a5e6003b3fc19a741e87b5ada3fe
dkwelcher <dkwelcher@gmail.com>
2024-10-30 18:11:28 -0400

    Added a constant module for section titles & text

---
+++ src/lib/constants/landing/LandingPageText.jsx
@@ -0,0 +1,18 @@
+export const SECTION1_DESCRIPTION =
+  "Fishing App's mission is to improve your fishing journeys by providing you with your own secure, personal database. Within the dashboard, you can create and manage your fishing trip history and record your catching successes. The upcoming feature, Trip Planner, will take your personal catch data and the forecasted weather to provide optimal fishing locations and strategies to enhance your fishing experience. Fishing App is your ultimate companion for catching more fish.";
+
+export const SECTION2_DESCRIPTION =
+  "This upcoming feature will help you catch more fish. Just select the date of your planned fishing trip, and the Fishing Trip Planner will do the rest. By combining your fishing trip history and the forecasted weather on the day of the planned trip, the Fishing Trip Planner algorithm will give you fishing recommendations and strategies to catch more fish.";
+
+export const SECTION3_DESCRIPTION =
+  "This upcoming feature will make your fishing trips more efficient. All time-, geographic-, & weather-related data will be automatically generated for you, leaving more time for catching your limit.";
+
+export const SUBSECTION_DESCRIPTIONS = [
+  SECTION2_DESCRIPTION,
+  SECTION3_DESCRIPTION,
+];
+
+export const SECTION2_TITLE = "Fishing Trip Planner";
+export const SECTION3_TITLE = "Auto-Generated Input";
+
+export const SUBSECTION_TITLES = [SECTION2_TITLE, SECTION3_TITLE];
