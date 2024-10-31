49b0c7f84cffb2a931375b88e20133721f844437
dkwelcher <dkwelcher@gmail.com>
2024-10-31 11:35:25 -0400

    Added constants module for signup rules

---
+++ src/lib/constants/auth/SignupRules.jsx
@@ -0,0 +1,7 @@
+const usernameRule =
+  "Username can contain letters & digits, but no spaces or special characters.";
+const emailRule = "Email must be a valid email address";
+const passwordRule =
+  "Password must contain at least one letter, one digit, & one special character";
+
+export const SIGNUP_RULES = [usernameRule, emailRule, passwordRule];
