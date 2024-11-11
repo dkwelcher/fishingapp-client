25630d6475c3ca6d64e2b8f91a87d2119aa571cc
dkwelcher <dkwelcher@gmail.com>
2024-11-11 11:15:19 -0500

    Removed styling abstraction. Renamed useState variable
    Tailwind utility classes are now stated in HTML instead of unnecessarily abstracting to variables. useState variable isOpen, setIsOpen renamed for clarity. Unnecessary comments removed.

--- src/components/dashboard/shared/Dashboard.jsx
+++ src/components/dashboard/shared/Dashboard.jsx
@@ -3,25 +3,12 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
-/* 
-Layout is a component that defines the layout of the dashboard. It dynamically renders the sidebar & the Outlet, which is a child of the layout component.
-It also handles sidebar behavior on mobile devices.
-
-@param setUser Setter function for user state passed to Sidebar component specifically for log out event.
-@return HTML rendering the sidebar and Outlet.
-*/
 function Dashboard({ setUser, screenWidth }) {
-  // isOpen state reflects the open / close status of the sidebar on mobile devices.
-  const [isOpen, setIsOpen] = useState(false);
-
-  // dropdownRef refers to an HTML element.
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  /*
-  toggleNavbar uses sets isOpen to the opposite of its current state.
-  */
   function toggleNavbar() {
-    setIsOpen(!isOpen);
+    setIsSidebarOpen(!isSidebarOpen);
   }
 
   /* 
@@ -43,7 +30,7 @@ function Dashboard({ setUser, screenWidth }) {
       }
 
       if (!isInsideDropdown) {
-        setIsOpen(false);
+        setIsSidebarOpen(false);
       }
     };
 
@@ -54,24 +41,18 @@ function Dashboard({ setUser, screenWidth }) {
     };
   }, []);
 
-  /* Tailwind Class Styles */
-  const pageStyles =
-    "flex flex-row bg-slate-100 h-screen w-screen overflow-hidden";
-  const viewStyles = "flex flex-col flex-1 overflow-auto";
-  /* End Tailwind Class Styles */
-
   return (
-    <div className={pageStyles}>
+    <div className="flex flex-row bg-slate-100 h-screen w-screen overflow-hidden">
       <div>
         <button
           className={`absolute w-full p-2 text-3xl text-slate-200 z-50 md:invisible ${
-            !isOpen && "bg-transparent-shadow"
+            !isSidebarOpen && "bg-transparent-shadow"
           }`}
           onClick={() => {
             toggleNavbar();
           }}
         >
-          {isOpen ? <RxCross1 /> : <RxHamburgerMenu />}
+          {isSidebarOpen ? <RxCross1 /> : <RxHamburgerMenu />}
         </button>
       </div>
       <div className="invisible absolute md:visible md:static">
@@ -83,7 +64,7 @@ function Dashboard({ setUser, screenWidth }) {
         <div
           ref={dropdownRef}
           className={`z-30 transition-all duration-700 ease-in-out ${
-            isOpen && screenWidth < 768
+            isSidebarOpen && screenWidth < 768
               ? "opacity-100 visible"
               : "opacity-0 invisible"
           }`}
@@ -91,7 +72,7 @@ function Dashboard({ setUser, screenWidth }) {
           <Sidebar setUser={setUser} screenWidth={screenWidth} />
         </div>
       </div>
-      <div className={viewStyles}>
+      <div className="flex flex-col flex-1 overflow-auto">
         <div>{<Outlet />}</div>
       </div>
     </div>
