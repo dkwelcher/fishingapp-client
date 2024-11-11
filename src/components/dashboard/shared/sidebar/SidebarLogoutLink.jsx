c8e4e7d0eddcd9ca99b2302775e514d914396c88
dkwelcher <dkwelcher@gmail.com>
2024-11-11 11:52:56 -0500

    Added sidebar-related component for logging out of dashboard

---
+++ src/components/dashboard/shared/sidebar/SidebarLogoutLink.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import { HiOutlineLogout } from "react-icons/hi";
+
+function SidebarLogoutLink({ setUser }) {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("authToken");
+    setUser({});
+    navigate("/");
+  }
+  return (
+    <div
+      className="text-red-600 cursor-pointer flex items-center gap-2 font-light px-3 py-2 hover:bg-slate-700 hover:no-underline active:bg-slate-600 rounded-sm text-base"
+      onClick={handleLogout}
+    >
+      <span className="text-xl">{<HiOutlineLogout />}</span>
+      Logout
+    </div>
+  );
+}
+
+export default SidebarLogoutLink;
