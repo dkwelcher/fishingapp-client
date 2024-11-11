680b4d997682a95c8fba9f4ed33853bed9bd7447
dkwelcher <dkwelcher@gmail.com>
2024-11-11 17:05:23 -0500

    Added component for text related to ManageTrips

---
+++ src/components/dashboard/views/managetrips/shared/ManageTripsText.jsx
@@ -0,0 +1,17 @@
+function ManageTripsText() {
+  return (
+    <>
+      <h1 className="font-title font-medium text-3xl pb-4 lg:text-4xl">
+        Manage Your Fishing Trips
+      </h1>
+      <p className="font-paragraph text-md pb-4 lg:w-[60%] 2xl:w-[40%]">
+        Create & manage your fishing trips. Select a date & add a new trip or
+        select an existing one. Then create & manage catches made on your
+        fishing trip. Catch records consist of relevant data such as fish
+        caught, geographic coordinates, and weather conditions.
+      </p>
+    </>
+  );
+}
+
+export default ManageTripsText;
