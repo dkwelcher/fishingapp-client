e8f5fba3cc54d22223ac56c1cb8d7d86eb15db27
dkwelcher <dkwelcher@gmail.com>
2024-11-11 12:56:37 -0500

    Added component for rendering Logo & descriptive text

---
+++ src/components/dashboard/views/feedback/shared/FeedbackContent.jsx
@@ -0,0 +1,13 @@
+import Logo from "./FeedbackLogo.jsx";
+import Text from "./FeedbackText.jsx";
+
+function FeedbackContent() {
+  return (
+    <>
+      <Logo />
+      <Text />
+    </>
+  );
+}
+
+export default FeedbackContent;
