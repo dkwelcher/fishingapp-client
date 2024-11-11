bec7de557bcecfbd5afa7e05b51792afc8211fd5
dkwelcher <dkwelcher@gmail.com>
2024-11-11 11:51:08 -0500

    Abstracted components
    Logo component contains app logo image & name. SidebarLink, formerly a nested component, now resides in its own component. LogoutLink does not adhere to SidebarLink mapping; thus, it was abstracted to its own component.

---
+++ src/components/dashboard/shared/sidebar/Sidebar.jsx
@@ -0,0 +1,32 @@
+import {
+  DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+  DASHBOARD_SIDEBAR_LINKS,
+} from "../../../../lib/constants/index";
+import Logo from "./SidebarLogo.jsx";
+import SidebarLink from "./SidebarLink.jsx";
+import LogoutLink from "./SidebarLogoutLink.jsx";
+
+function Sidebar({ setUser, screenWidth }) {
+  return (
+    <div
+      className={`pt-10 h-full bg-slate-950 w-60 p-3 flex flex-col border-r border-slate-700 md:pt-0 ${
+        screenWidth < 768 && "absolute z-40"
+      }`}
+    >
+      <Logo />
+      <div className="flex-1 py-8 flex flex-col gap-0.5">
+        {DASHBOARD_SIDEBAR_LINKS.map((item) => (
+          <SidebarLink key={item.key} item={item} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-0.5 pt-2 border-t border-slate-700">
+        {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
+          <SidebarLink key={item.key} item={item} />
+        ))}
+        <LogoutLink setUser={setUser} />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
