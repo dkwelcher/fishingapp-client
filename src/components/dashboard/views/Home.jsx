8e3bc3dd3f16b76265b05b7f0ba05a4de78c7793
dkwelcher <dkwelcher@gmail.com>
2024-11-11 12:04:27 -0500

    Moved styling to html. Removed unnecessary comments

--- src/components/dashboard/views/Home.jsx
+++ src/components/dashboard/views/Home.jsx
@@ -1,24 +1,11 @@
-/* 
-Home.jsx is a dashboard component that displays a background image & the SearchBar component.
-
-@since 2024-03-18
-*/
-
 import SearchBar from "../shared/SearchBar";
 
-/* 
-Home renders a background image and a SearchBar component.
-
-@param user An object that holds user properties.
-@return HTML that renders a container with a background image & a SearchBar component.
-*/
 function Home({ user }) {
-  /* Tailwind Class Styles */
-  const pageStyles =
-    "flex justify-center items-center h-screen bg-home-image bg-cover bg-center";
-  /* End Tailwind Class Styles */
-
-  return <div className={pageStyles}>{/* <SearchBar /> */}</div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-home-image bg-cover bg-center">
+      {<SearchBar />}
+    </div>
+  );
 }
 
 export default Home;
