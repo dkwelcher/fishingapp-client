c4d04a65b3f88d9d84a1ddd9791eb8ef7aac188e
dkwelcher <dkwelcher@gmail.com>
2024-11-18 12:41:20 -0500

    Moved container elements to parent. Added button component for managing trips

--- src/components/dashboard/views/managetrips/cards/trip/TripCard.jsx
+++ src/components/dashboard/views/managetrips/cards/trip/TripCard.jsx
@@ -1,10 +1,9 @@
 import TripDetails from "./TripDetails.jsx";
-import TripButtons from "./TripButtons.jsx";
+import Button from "./shared/TripCardButton.jsx";
 import CatchCards from "../catch/CatchCards.jsx";
 
 function TripCard({
   trip,
-  setTempTrip,
   catches,
   setCatches,
   setTempCatch,
@@ -14,38 +13,45 @@ function TripCard({
   setOpenEditCatchModal,
   openDeleteCatchModal,
   setOpenDeleteCatchModal,
-  setOpenEditTripModal,
-  setOpenDeleteTripModal,
   screenWidth,
+  handleEditTripClick,
+  handleDeleteTripClick,
 }) {
   return (
-    <div className="py-4 sm:p-4 lg:py6 2xl:py-10">
-      <div className="p-4 rounded-sm bg-slate-800 shadow-md shadow-slate-800">
-        <div className="mb-4 pb-4 flex justify-between border-0 border-b-4 border-slate-300">
+    <div className="p-4 rounded-sm bg-slate-800 shadow-md shadow-slate-800">
+      <div className="mb-4 pb-4 flex justify-between border-0 border-b-4 border-slate-300">
+        <div className="w-full flex flex-col items-start text-white">
           <TripDetails trip={trip} />
-          <TripButtons
-            trip={trip}
-            setTempTrip={setTempTrip}
-            setOpenEditTripModal={setOpenEditTripModal}
-            setOpenDeleteTripModal={setOpenDeleteTripModal}
+        </div>
+        <div className="w-full flex justify-end items-center gap-4">
+          <Button
+            buttonTextShort={"Edit"}
+            buttonTextLong={"Edit Current Trip"}
             screenWidth={screenWidth}
+            handleClick={handleEditTripClick}
+          />
+          <Button
+            buttonTextShort={"Delete"}
+            buttonTextLong={"Delete Current Trip"}
+            screenWidth={screenWidth}
+            handleClick={handleDeleteTripClick}
           />
         </div>
-        <div className="px-6">
-          {trip.location && trip.date && (
-            <CatchCards
-              catches={catches}
-              setCatches={setCatches}
-              setTempCatch={setTempCatch}
-              openAddCatchModal={openAddCatchModal}
-              setOpenAddCatchModal={setOpenAddCatchModal}
-              openEditCatchModal={openEditCatchModal}
-              setOpenEditCatchModal={setOpenEditCatchModal}
-              openDeleteCatchModal={openDeleteCatchModal}
-              setOpenDeleteCatchModal={setOpenDeleteCatchModal}
-            />
-          )}
-        </div>
+      </div>
+      <div className="px-6">
+        {trip.location && trip.date && (
+          <CatchCards
+            catches={catches}
+            setCatches={setCatches}
+            setTempCatch={setTempCatch}
+            openAddCatchModal={openAddCatchModal}
+            setOpenAddCatchModal={setOpenAddCatchModal}
+            openEditCatchModal={openEditCatchModal}
+            setOpenEditCatchModal={setOpenEditCatchModal}
+            openDeleteCatchModal={openDeleteCatchModal}
+            setOpenDeleteCatchModal={setOpenDeleteCatchModal}
+          />
+        )}
       </div>
     </div>
   );
