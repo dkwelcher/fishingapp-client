872df06cca221d113a56199110b62a3fc20015c1
dkwelcher <dkwelcher@gmail.com>
2024-11-18 14:10:39 -0500

    onClick behavior condensed to one function

--- src/components/dashboard/views/managetrips/cards/catch/CatchButton.jsx
+++ src/components/dashboard/views/managetrips/cards/catch/CatchButton.jsx
@@ -1,12 +1,11 @@
-function CatchButton({ buttonText, index, setOpenModal, handleCatch }) {
+function CatchButton({ buttonText, index, handleClick }) {
   return (
     <button
       className="bg-slate-800 text-slate-200 px-6 py-2 rounded-sm hover:bg-slate-700 shadow-md shadow-slate-900"
       data-key={index}
       onClick={(e) => {
         const dataKey = e.currentTarget.getAttribute("data-key");
-        setOpenModal(true);
-        handleCatch(dataKey);
+        handleClick(dataKey);
       }}
     >
       {buttonText}
