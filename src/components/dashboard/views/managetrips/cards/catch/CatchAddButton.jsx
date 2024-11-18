4eb137d9db949a815d92c6b793fa5ce1a8aae9a6
dkwelcher <dkwelcher@gmail.com>
2024-11-18 13:38:17 -0500

    Modularized the function param

--- src/components/dashboard/views/managetrips/cards/catch/CatchAddButton.jsx
+++ src/components/dashboard/views/managetrips/cards/catch/CatchAddButton.jsx
@@ -1,9 +1,9 @@
-function CatchAddButton({ setOpenAddCatchModal }) {
+function CatchAddButton({ handleClick }) {
   return (
     <div className="pb-2 flex flex-col items-center">
       <button
         className="w-[300px] bg-slate-300 text-slate-600 px-6 py-2 rounded-sm font-bold shadow-md shadow-slate-900 hover:bg-slate-100 hover:text-slate-500"
-        onClick={() => setOpenAddCatchModal(true)}
+        onClick={() => handleClick()}
       >
         Add a New Catch
       </button>
