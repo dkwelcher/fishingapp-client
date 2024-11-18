c73b93d8bfc7db9aecdbc4e357e4d77c1b7cff13
dkwelcher <dkwelcher@gmail.com>
2024-11-18 14:12:33 -0500

    onClick functions moved to parent
    Allows multiple states & functions to be handled in parent component instead of passing separately to this component.

--- src/components/dashboard/views/managetrips/cards/trip/TripCard.jsx
+++ src/components/dashboard/views/managetrips/cards/trip/TripCard.jsx
@@ -6,37 +6,13 @@ import CatchCard from "./../catch/CatchCard.jsx";
 function TripCard({
   trip,
   catches,
-  setTempCatch,
-  setOpenAddCatchModal,
-  setOpenEditCatchModal,
-  setOpenDeleteCatchModal,
   screenWidth,
   handleEditTripClick,
   handleDeleteTripClick,
+  handleAddCatchClick,
+  handleEditCatchClick,
+  handleDeleteCatchClick,
 }) {
-  function handleAddCatchClick() {
-    setOpenAddCatchModal(true);
-  }
-
-  function handleEditCatch(dataKey) {
-    const catchItem = catches[dataKey];
-    const newCatchItem = { ...catchItem, index: dataKey };
-    setTempCatch(newCatchItem);
-  }
-
-  function handleDeleteCatch(dataKey) {
-    const catchItem = catches[dataKey];
-    setTempCatch(catchItem);
-  }
-
-  function handleTimeConversionTo12HourFormat(time) {
-    const [hours, minutes] = time.split(":");
-    const hoursInt = parseInt(hours, 10);
-    const suffix = hoursInt >= 12 ? "pm" : "am";
-    const newHours = ((hoursInt + 11) % 12) + 1;
-    return `${newHours.toString().padStart(2, "0")}:${minutes} ${suffix}`;
-  }
-
   return (
     <div className="p-4 rounded-sm bg-slate-800 shadow-md shadow-slate-800">
       <div className="mb-4 pb-4 flex justify-between border-0 border-b-4 border-slate-300">
@@ -68,13 +44,8 @@ function TripCard({
                   key={catchItem.id}
                   catchItem={catchItem}
                   index={index}
-                  handleEditCatch={handleEditCatch}
-                  handleDeleteCatch={handleDeleteCatch}
-                  handleTimeConversionTo12HourFormat={
-                    handleTimeConversionTo12HourFormat
-                  }
-                  setOpenEditCatchModal={setOpenEditCatchModal}
-                  setOpenDeleteCatchModal={setOpenDeleteCatchModal}
+                  handleEditCatchClick={handleEditCatchClick}
+                  handleDeleteCatchClick={handleDeleteCatchClick}
                 />
               ))}
             </div>
