c50c67ea59936dc768f7c08413ab3b8b85aa055a
dkwelcher <dkwelcher@gmail.com>
2024-11-18 14:13:15 -0500

    onClick functions moved to parent
    Allows multiple states & functions to be handled in parent component instead of passing separately to this component.

--- src/components/dashboard/views/managetrips/shared/LastSixMonthsButton.jsx
+++ src/components/dashboard/views/managetrips/shared/LastSixMonthsButton.jsx
@@ -1,12 +1,8 @@
-function LastSixMonthsButton({ setTripDate, setTrip }) {
-  function handleLastSixMonthsClick() {
-    setTripDate();
-    setTrip({});
-  }
+function LastSixMonthsButton({ handleClick }) {
   return (
     <button
       className="px-4 py-1 bg-slate-50 text-slate-800 rounded-sm shadow-md shadow-slate-950 text-sm sm:text-base"
-      onClick={() => handleLastSixMonthsClick()}
+      onClick={() => handleClick()}
     >
       Show Last Six Months
     </button>
