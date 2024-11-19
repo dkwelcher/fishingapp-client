0c9887443cadafe5cceb8e4ed4594fd9b26168e0
dkwelcher <dkwelcher@gmail.com>
2024-11-19 13:25:51 -0500

    Added id to input element

--- src/components/dashboard/shared/SearchBar.jsx
+++ src/components/dashboard/shared/SearchBar.jsx
@@ -5,6 +5,7 @@ function SearchBar() {
     <div className="relative">
       <HiOutlineSearch className="text-gray-400 absolute top-1/2 -translate-y-1/2 left-3 size-5" />
       <input
+        id="search-dashboard"
         className="text-sm focus:outline-none active:outline-none h-10 w-[18rem] border border-gray-300 rounded-sm pl-11 pr-4 md:w-[24rem]"
         type="text"
         placeholder="Search..."
