991eb15b533116cdc703fe3db4e5fa11223f5e72
dkwelcher <dkwelcher@gmail.com>
2024-11-19 15:59:30 -0500

    Added ScreenWidthContext to hold dynamic screenWidth values

--- src/lib/context/Context.jsx
+++ src/lib/context/Context.jsx
@@ -2,3 +2,4 @@ import { createContext } from "react";
 
 export const BaseURLContext = createContext();
 export const AuthContext = createContext();
+export const ScreenWidthContext = createContext();
