8ccf61b3f3600a0afb13ed81e020bc8bb91458a4
dkwelcher <dkwelcher@gmail.com>
2024-11-20 13:35:43 -0500

    Refactored code. Removed unnecessary useEffect

--- src/components/dashboard/views/feedback/Feedback.jsx
+++ src/components/dashboard/views/feedback/Feedback.jsx
@@ -1,97 +1,82 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useContext } from "react";
+import { FEEDBACK_POST_REQUEST } from "../../../../lib/http/PostRequests.jsx";
 import Content from "./shared/FeedbackContent.jsx";
 import Form from "./shared/FeedbackForm.jsx";
-import { BaseURLContext } from "../../../../lib/context/Context.jsx";
 import { AuthContext } from "../../../../lib/context/Context.jsx";
 
 function Feedback() {
-  const baseURL = useContext(BaseURLContext);
   const { user, setUser } = useContext(AuthContext);
 
-  const [userInput, setUserInput] = useState("");
+  const [feedback, setFeedback] = useState("");
   const [selectPage, setSelectPage] = useState("Landing page");
-  const [hasSubmittedFeedback, setHasSubmittedFeedback] = useState(false);
   const [isSuccessfulSubmission, setIsSuccessfulSubmission] = useState(false);
   const [formSubmissionErrorMessage, setFormSubmissionErrorMessage] =
     useState("");
 
-  /* 
-    The useEffect detects if userInput is truthy & hasSubmittedFeedback is true, then calls postFeedback(), & sets hasSubmittedFeedback state to false.
-    Its purpose is to ensure the userInput state is updated before sending a POST request to the server.
-  */
-  useEffect(() => {
-    if (hasSubmittedFeedback && userInput) {
-      postFeedback();
-      setHasSubmittedFeedback(false);
-    }
-  }, [userInput, hasSubmittedFeedback]);
-
   function handleFeedback() {
-    const trimmedFeedback = userInput.trim();
-
-    if (!trimmedFeedback) {
+    if (!feedback) {
       setFormSubmissionErrorMessage("Feedback cannot be empty");
       return;
     }
 
     const MAX_LENGTH_SUBMISSION = 1000;
-    if (trimmedFeedback.length > MAX_LENGTH_SUBMISSION) {
+    if (feedback.length > MAX_LENGTH_SUBMISSION) {
       setFormSubmissionErrorMessage(
-        `Feedback cannot exceed ${MAX_LENGTH_SUBMISSION} words`
+        `Feedback cannot exceed ${MAX_LENGTH_SUBMISSION} characters`
       );
       return;
     }
 
-    formatFeedback();
-    setHasSubmittedFeedback(true);
+    const formattedFeedback = formatFeedback();
+    setFeedback(formattedFeedback);
+    postFeedback(formattedFeedback);
   }
 
   function formatFeedback() {
-    const currentTime = new Date().toLocaleTimeString("it-IT", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-    setUserInput(
-      (prevFeedback) =>
-        `user=${user.id} feedbackOn=${selectPage} (${currentTime}): ${prevFeedback}`
-    );
+    const currentDate = new Date();
+    return `user=${
+      user.id
+    } feedbackOn=${selectPage} (${currentDate}): ${feedback.trim()}`;
   }
 
-  async function postFeedback() {
-    const POST_FEEDBACK = `${baseURL}/feedback?userId=${user.id}`;
-    const token = localStorage.getItem("authToken");
-
+  async function postFeedback(formattedFeedback) {
     try {
-      const response = await fetch(POST_FEEDBACK, {
+      const response = await fetch(FEEDBACK_POST_REQUEST(user.id), {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(userInput),
+        body: JSON.stringify(formattedFeedback),
       });
 
       if (!response.ok) {
-        setFormSubmissionErrorMessage("Something went wrong. Please try again");
-      } else {
-        setUserInput("");
-        setSelectPage("landing page");
-        setFormSubmissionErrorMessage("");
-        setIsSuccessfulSubmission(true);
+        setFormSubmissionErrorMessage("Something went wrong");
+        throw new Error(`HTTP error: ${response.error}`);
       }
+
+      resetForm();
+      setIsSuccessfulSubmission(true);
     } catch (error) {
       console.log(error);
     }
   }
 
-  function handleClearSubmissionConfirmation() {
+  function resetForm() {
+    setFeedback("");
+    setSelectPage("landing page");
+    setFormSubmissionErrorMessage("");
+  }
+
+  function handleResetNotifications() {
     setIsSuccessfulSubmission(false);
+    setFormSubmissionErrorMessage("");
   }
 
   return (
     <div className="p-4 flex flex-col h-screen justify-center items-center font-paragraph font-normal text-sm lg:text-base text-slate-200 bg-feedback-image-mobile lg:bg-feedback-image-desktop bg-cover bg-center">
       <p className="absolute bottom-1 right-1 text-xs 2xl:text-sm">
-        Image credit: Hala Strohmier Berry
+        Image credit: Dr. Hala Strohmier Berry
       </p>
       <div
         className={`max-w-[500px] w-full lg:mb-2 py-1 bg-transparent-shadow-feedback-success rounded-sm ${
@@ -105,12 +90,12 @@ function Feedback() {
       <div className="p-8 max-w-[500px] rounded-sm bg-transparent-shadow-darker lg:bg-transparent-shadow shadow-md shadow-slate-900">
         <Content />
         <Form
-          userInput={userInput}
-          setUserInput={setUserInput}
+          userInput={feedback}
+          setUserInput={setFeedback}
           selectPage={selectPage}
           setSelectPage={setSelectPage}
           formSubmissionErrorMessage={formSubmissionErrorMessage}
-          handleClearSubmissionConfirmation={handleClearSubmissionConfirmation}
+          handleResetNotifications={handleResetNotifications}
           handleFeedback={handleFeedback}
         />
       </div>
