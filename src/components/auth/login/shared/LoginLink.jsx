9747c19e5059365507448bead3f0d7d74e0c93d5
dkwelcher <dkwelcher@gmail.com>
2024-11-20 11:04:59 -0500

    Renamed function prop name

--- src/components/auth/login/shared/LoginLink.jsx
+++ src/components/auth/login/shared/LoginLink.jsx
@@ -1,11 +1,11 @@
-function LoginLink({ handleSignupEntry }) {
+function LoginLink({ handleNavigation }) {
   return (
     <p className="text-center">
       Need to
       <a
         className="pl-1 underline text-slate-200 cursor-pointer hover:no-underline active:no-underline"
         onClick={() => {
-          handleSignupEntry();
+          handleNavigation();
         }}
       >
         create an account
