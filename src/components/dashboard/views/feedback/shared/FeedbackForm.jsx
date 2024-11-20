d6f02e8b9b433848023540f90459958a4581932e
dkwelcher <dkwelcher@gmail.com>
2024-11-20 13:37:51 -0500

    Abstracted select element to component

--- src/components/dashboard/views/feedback/shared/FeedbackForm.jsx
+++ src/components/dashboard/views/feedback/shared/FeedbackForm.jsx
@@ -1,45 +1,45 @@
+import SelectBox from "./FeedbackSelectBox";
+
 function FeedbackForm({
   userInput,
   setUserInput,
   selectPage,
   setSelectPage,
   formSubmissionErrorMessage,
-  handleClearSubmissionConfirmation,
+  handleResetNotifications,
   handleFeedback,
 }) {
+  const optionValues = [
+    "landing-page",
+    "signup-page",
+    "login-page",
+    "home-page",
+    "manage-trips-page",
+    "other",
+  ];
   return (
     <>
       <form htmlFor="feedback" className="w-full min-h-[180px]">
-        <div className="flex flex-col gap-y-1">
-          <p>Please select which part of the app:</p>
-          <select
-            id="select-page"
-            className="px-2 py-1 bg-slate-50 text-slate-800 border border-solid border-slate-400 rounded-sm shadow-sm shadow-slate-600 focus:bg-slate-200 focus:text-slate-900 outline-none"
-            value={selectPage}
-            type="text"
-            onChange={(e) => setSelectPage(e.target.value)}
-          >
-            <option value="landing-page">Landing page</option>
-            <option value="signup-page">Signup page</option>
-            <option value="login-page">Login page</option>
-            <option value="home-page">Home page</option>
-            <option value="manage-trips-page">Manage Trips page</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
+        <p className="mb-1">Please select which part of the app:</p>
+        <SelectBox
+          elementId={"select-page"}
+          elementValue={selectPage}
+          handleOnChange={setSelectPage}
+          optionValuesArray={optionValues}
+        />
         <textarea
-          id="feedback"
           className="w-full min-h-[180px] mt-1 p-1 text-slate-800 shadow-sm shadow-slate-600 rounded-sm"
+          id="feedback"
           value={userInput}
           onChange={(e) => {
-            handleClearSubmissionConfirmation();
             setUserInput(e.target.value);
           }}
+          onBlur={() => handleResetNotifications()}
         ></textarea>
       </form>
-      <div>
+      <div className="my-2">
         <p
-          className={`py-2 text-red-600 text-center ${
+          className={`text-red-600 text-center ${
             formSubmissionErrorMessage.length > 0 ? "visible" : "hidden"
           }`}
         >
