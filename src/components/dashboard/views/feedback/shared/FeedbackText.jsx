6f24fc4e2c710e6555e0a588a9f62880a731c60e
dkwelcher <dkwelcher@gmail.com>
2024-11-20 13:36:45 -0500

    Moved container positioning to parent. Removed unnecessary styles

--- src/components/dashboard/views/feedback/shared/FeedbackText.jsx
+++ src/components/dashboard/views/feedback/shared/FeedbackText.jsx
@@ -1,15 +1,13 @@
 function FeedbackText() {
   return (
-    <div className="pb-8">
+    <>
       <h2 className="pb-2 font-title font-semibold text-lg">FEEDBACK</h2>
-      <div className="text-slate-300">
-        <p className="pb-2 last:pb-0">
-          If you are reporting a bug, then please provide as many specific
-          details as you can. Otherwise, feel free to comment on what you do &
-          don't like about the app.
-        </p>
-      </div>
-    </div>
+      <p>
+        If you are reporting a bug, then please provide as many specific details
+        as you can. Otherwise, feel free to comment on what you do & don't like
+        about the app.
+      </p>
+    </>
   );
 }
 
