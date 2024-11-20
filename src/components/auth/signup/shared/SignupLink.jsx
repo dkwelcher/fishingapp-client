d6354cc5fe37e3831e291eec6f567cf109369939
dkwelcher <dkwelcher@gmail.com>
2024-11-20 12:00:31 -0500

    Renamed prop

--- src/components/auth/signup/shared/SignupLink.jsx
+++ src/components/auth/signup/shared/SignupLink.jsx
@@ -1,11 +1,11 @@
-function SignupLink({ handleNavigateToLogin }) {
+function SignupLink({ handleNavigation }) {
   return (
     <p className="text-center">
       Already have an account?
       <a
         className="pl-1 underline text-slate-200 cursor-pointer hover:no-underline active:no-underline"
         onClick={() => {
-          handleNavigateToLogin();
+          handleNavigation();
         }}
       >
         Log in here
