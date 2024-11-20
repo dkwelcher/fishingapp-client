1090cd5ed4da9da26d6c7fdf5da1743d5f00b034
dkwelcher <dkwelcher@gmail.com>
2024-11-20 13:39:11 -0500

    Added function for returning a post request endpoint specific to feedback

--- src/lib/http/PostRequests.jsx
+++ src/lib/http/PostRequests.jsx
@@ -5,3 +5,8 @@ export const AUTHENTICATE_USER_POST_REQUEST = `${baseURL}/auth/authenticate`;
 
 /* SIGNUP */
 export const REGISTER_USER_POST_REQUEST = `${baseURL}/auth/register`;
+
+/* FEEDBACK */
+export const FEEDBACK_POST_REQUEST = (userId) => {
+  return `${baseURL}/feedback?userId=${userId}`;
+};
