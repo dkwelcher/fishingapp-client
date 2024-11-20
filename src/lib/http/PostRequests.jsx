75b5940cd04ebacd0e413b8c0bd0c700ad296b58
dkwelcher <dkwelcher@gmail.com>
2024-11-20 12:01:31 -0500

    Added endpoint for registering user with post request

--- src/lib/http/PostRequests.jsx
+++ src/lib/http/PostRequests.jsx
@@ -2,3 +2,6 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 /* LOGIN */
 export const AUTHENTICATE_USER_POST_REQUEST = `${baseURL}/auth/authenticate`;
+
+/* SIGNUP */
+export const REGISTER_USER_POST_REQUEST = `${baseURL}/auth/register`;
