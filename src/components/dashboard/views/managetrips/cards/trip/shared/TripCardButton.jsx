ec3058587b57e25f7d4d509a46573982b63fd1af
dkwelcher <dkwelcher@gmail.com>
2024-11-21 12:12:15 -0500

    Reordered styles for consistency
    Classes that affect positioning are ordered at beginning.

--- src/components/dashboard/views/managetrips/cards/trip/shared/TripCardButton.jsx
+++ src/components/dashboard/views/managetrips/cards/trip/shared/TripCardButton.jsx
@@ -7,7 +7,7 @@ function TripCardButton({ buttonTextShort, buttonTextLong, handleClick }) {
 
   return (
     <button
-      className="bg-slate-300 text-slate-600 px-2 py-1 rounded-sm font-paragraph font-bold shadow-md shadow-slate-900 hover:bg-slate-100 hover:text-slate-500 sm:px-4 sm:py-2"
+      className="px-2 py-1 rounded-sm font-paragraph font-bold bg-slate-300 text-slate-600 shadow-md shadow-slate-900 hover:bg-slate-100 hover:text-slate-500 sm:px-4 sm:py-2"
       onClick={() => {
         handleClick();
       }}
