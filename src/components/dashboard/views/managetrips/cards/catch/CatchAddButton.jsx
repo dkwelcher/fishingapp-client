72a5c629ebb4ceaf9ea476989bf181acc7277e44
dkwelcher <dkwelcher@gmail.com>
2024-11-21 12:09:30 -0500

    Moved container element to parent

--- src/components/dashboard/views/managetrips/cards/catch/CatchAddButton.jsx
+++ src/components/dashboard/views/managetrips/cards/catch/CatchAddButton.jsx
@@ -1,13 +1,11 @@
 function CatchAddButton({ handleClick }) {
   return (
-    <div className="pb-2 flex flex-col items-center">
-      <button
-        className="w-[300px] bg-slate-300 text-slate-600 px-6 py-2 rounded-sm font-bold shadow-md shadow-slate-900 hover:bg-slate-100 hover:text-slate-500"
-        onClick={() => handleClick()}
-      >
-        Add a New Catch
-      </button>
-    </div>
+    <button
+      className="w-[300px] bg-slate-300 text-slate-600 px-6 py-2 rounded-sm font-bold shadow-md shadow-slate-900 hover:bg-slate-100 hover:text-slate-500"
+      onClick={() => handleClick()}
+    >
+      Add a New Catch
+    </button>
   );
 }
 
