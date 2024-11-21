a2b999405e6d43ca5af09e6f1a7f67477babbe19
dkwelcher <dkwelcher@gmail.com>
2024-11-21 12:18:46 -0500

    Reordered styles
    Styles that affect positioning are ordered at the beginning.

--- src/components/landing/shared/hero/Hero.jsx
+++ src/components/landing/shared/hero/Hero.jsx
@@ -4,13 +4,13 @@ import Content from "./HeroContent.jsx";
 function Hero({ handleLoginEntry, handleSignupEntry }) {
   return (
     <>
-      <header className="flex justify-between p-2 lg:p-4">
+      <header className="p-2 flex justify-between lg:p-4">
         <Heading
           handleLoginEntry={handleLoginEntry}
           handleSignupEntry={handleSignupEntry}
         />
       </header>
-      <div className="p-2 flex flex-col justify-center items-center gap-y-2 h-4/5 md:gap-y-4">
+      <div className="h-4/5 p-2 flex flex-col justify-center items-center gap-y-2 md:gap-y-4">
         <Content handleSignupEntry={handleSignupEntry} />
       </div>
     </>
